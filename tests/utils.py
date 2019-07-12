4c0b079b9024f71e12f0e2e259fd6c7e8c44107e
Azat Ibrakov <azatibrakov@gmail.com>
2019-07-12 21:51:38 +0300

    Complete test for 'Polygon.is_convex' predicate

--- tests/utils.py
+++ tests/utils.py
@@ -3,5 +3,9 @@ from hypothesis.searchstrategy import SearchStrategy
 Strategy = SearchStrategy
 
 
+def equivalence(left_statement: bool, right_statement: bool) -> bool:
+    return not left_statement ^ right_statement
+
+
 def implication(antecedent: bool, consequent: bool) -> bool:
     return not antecedent or consequent
