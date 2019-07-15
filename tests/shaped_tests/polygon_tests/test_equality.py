975cc91170f63c817528a6181989ba52e905ed46
Azat Ibrakov <azatibrakov@gmail.com>
2019-07-15 21:32:39 +0300

    Add test for 'Polygon.__eq__' magic method

---
+++ tests/shaped_tests/polygon_tests/test_equality.py
@@ -0,0 +1,25 @@
+from hypothesis import given
+
+from gon.shaped import Polygon
+from tests import strategies
+from tests.utils import implication
+
+
+@given(strategies.polygons)
+def test_reflexivity(polygon: Polygon) -> None:
+    assert polygon == polygon
+
+
+@given(strategies.polygons, strategies.polygons)
+def test_symmetry(left_polygon: Polygon, right_polygon: Polygon) -> None:
+    assert implication(left_polygon == right_polygon,
+                       right_polygon == left_polygon)
+
+
+@given(strategies.polygons, strategies.polygons, strategies.polygons)
+def test_transitivity(left_polygon: Polygon,
+                      mid_polygon: Polygon,
+                      right_polygon: Polygon) -> None:
+    assert implication(left_polygon == mid_polygon
+                       and mid_polygon == right_polygon,
+                       left_polygon == right_polygon)
