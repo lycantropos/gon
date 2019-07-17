667f6c70d2fab2b6fa70029ba9a345a411c3486a
Azat Ibrakov <azatibrakov@gmail.com>
2019-07-17 19:30:08 +0300

    Complete tests configurations: decrease attempts count

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,7 @@ from hypothesis import (HealthCheck,
                         settings)
 
 settings.register_profile('default',
+                          max_examples=20,
                           deadline=None,
                           suppress_health_check=[HealthCheck.filter_too_much,
                                                  HealthCheck.too_slow])
