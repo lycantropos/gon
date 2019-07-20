72e82911fb69c25797a4a10491d39152acf45439
Ibrakov Azat <azatibrakov@gmail.com>
2019-07-20 22:04:38 +0300

    Complete strategies

--- tests/utils_tests/strategies.py
+++ tests/utils_tests/strategies.py
@@ -2,6 +2,6 @@ from hypothesis import strategies
 
 permutations = (strategies.builds(range,
                                   strategies.integers(min_value=1,
-                                                      max_value=10 ** 4))
+                                                      max_value=1000))
                 .flatmap(strategies.permutations))
 permutations |= permutations.map(tuple)
