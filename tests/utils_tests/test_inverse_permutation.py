18f65afa9e95a875a033a14e8d8611e048688361
Ibrakov Azat <azatibrakov@gmail.com>
2019-07-21 09:34:29 +0300

    Complete test for 'inverse_permutation' utility function

--- tests/utils_tests/test_inverse_permutation.py
+++ tests/utils_tests/test_inverse_permutation.py
@@ -5,6 +5,11 @@ from gon.utils import inverse_permutation
 from . import strategies
 
 
+@given(strategies.identity_permutations)
+def test_identity(permutation: Permutation) -> None:
+    assert inverse_permutation(permutation) == permutation
+
+
 @given(strategies.permutations)
 def test_involution(permutation: Permutation) -> None:
     assert inverse_permutation(inverse_permutation(permutation)) == permutation
