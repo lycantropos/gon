cfe167f9acbc340212e1934efe12d6d26c80662b
Ibrakov Azat <azatibrakov@gmail.com>
2019-07-21 09:34:29 +0300

    Complete test for 'inverse_permutation' utility function

--- tests/utils_tests/strategies.py
+++ tests/utils_tests/strategies.py
@@ -1,7 +1,9 @@
 from hypothesis import strategies
 
-permutations = (strategies.builds(range,
-                                  strategies.integers(min_value=1,
-                                                      max_value=1000))
-                .flatmap(strategies.permutations))
+identity_permutations = strategies.builds(range,
+                                          strategies.integers(min_value=1,
+                                                              max_value=1000))
+identity_permutations = (identity_permutations.map(list)
+                         | identity_permutations.map(tuple))
+permutations = identity_permutations.flatmap(strategies.permutations)
 permutations |= permutations.map(tuple)
