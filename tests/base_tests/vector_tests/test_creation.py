82c443e206f0fb2b4f8716fdb14f144713e45a96
Azat Ibrakov <azatibrakov@gmail.com>
2019-07-22 13:50:05 +0300

    Add tests for 'Vector' class

---
+++ tests/base_tests/vector_tests/test_creation.py
@@ -0,0 +1,18 @@
+import pytest
+from hypothesis import given
+
+from gon.base import Vector
+from gon.hints import Scalar
+from . import strategies
+
+
+@given(strategies.invalid_coordinates, strategies.valid_coordinates)
+def test_invalid_x_coordinate(x: Scalar, y: Scalar) -> None:
+    with pytest.raises(ValueError):
+        Vector(x, y)
+
+
+@given(strategies.valid_coordinates, strategies.invalid_coordinates)
+def test_invalid_y_coordinate(x: Scalar, y: Scalar) -> None:
+    with pytest.raises(ValueError):
+        Vector(x, y)
