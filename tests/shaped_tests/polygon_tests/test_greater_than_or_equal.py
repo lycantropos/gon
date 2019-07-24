173740b6d6ce08c6e0f2033af47929aed37d7ae5
Azat Ibrakov <azatibrakov@gmail.com>
2019-07-24 15:10:24 +0300

    Complete test for 'Polygon.__ge__' magic method

--- tests/shaped_tests/polygon_tests/test_greater_than_or_equal.py
+++ tests/shaped_tests/polygon_tests/test_greater_than_or_equal.py
@@ -25,6 +25,18 @@ def test_transitivity(left_polygon: Polygon,
                        left_polygon >= right_polygon)
 
 
+@given(strategies.polygons)
+def test_convex_hull(polygon: Polygon) -> None:
+    assert polygon.convex_hull >= polygon
+
+
+@given(strategies.polygons, strategies.polygons)
+def test_relation_with_greater_than(left_polygon: Polygon,
+                                    right_polygon: Polygon) -> None:
+    assert implication(left_polygon > right_polygon,
+                       left_polygon >= right_polygon)
+
+
 @given(strategies.polygons, strategies.polygons)
 def test_relation_with_issuperset(left_polygon: Polygon,
                                   right_polygon: Polygon) -> None:
