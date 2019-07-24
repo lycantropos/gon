49885b5f89796aaa81bfb865919235742ba4d76b
Azat Ibrakov <azatibrakov@gmail.com>
2019-07-24 17:18:27 +0300

    Complete strategies for 'to_polygon' factory function

--- tests/strategies/__init__.py
+++ tests/strategies/__init__.py
@@ -3,6 +3,5 @@ from .base import (points,
                    scalars_strategies,
                    scalars_to_points)
 from .linear import segment_to_scalars
-from .shaped import (invalid_vertices,
-                     to_non_triangle_vertices_base,
+from .shaped import (to_non_triangle_vertices_base,
                      triangles_vertices)
--- tests/strategies/shaped.py
+++ tests/strategies/shaped.py
@@ -4,11 +4,9 @@ from operator import attrgetter
 from hypothesis import strategies
 from lz.functional import (compose,
                            pack)
-from lz.logical import negate
 from lz.replication import replicator
 
-from gon.shaped import (self_intersects,
-                        vertices_forms_angles)
+from gon.shaped import vertices_forms_angles
 from .base import points_strategies
 
 triangles_vertices = (points_strategies
@@ -18,8 +16,3 @@ to_non_triangle_vertices_base = partial(strategies.lists,
                                         min_size=4,
                                         unique_by=(attrgetter('x'),
                                                    attrgetter('y')))
-invalid_vertices = points_strategies.flatmap(to_non_triangle_vertices_base)
-invalid_vertices = (points_strategies.flatmap(partial(strategies.lists,
-                                                      max_size=2))
-                    | invalid_vertices.filter(self_intersects)
-                    | invalid_vertices.filter(negate(vertices_forms_angles)))
