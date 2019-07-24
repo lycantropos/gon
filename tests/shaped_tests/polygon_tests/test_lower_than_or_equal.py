5a57e042f1df84db3f7aae3d7e68a4de291fcd06
Azat Ibrakov <azatibrakov@gmail.com>
2019-07-24 15:10:29 +0300

    Complete test for 'Polygon.__le__' magic method

--- tests/shaped_tests/polygon_tests/test_lower_than_or_equal.py
+++ tests/shaped_tests/polygon_tests/test_lower_than_or_equal.py
@@ -25,6 +25,18 @@ def test_transitivity(left_polygon: Polygon,
                        left_polygon <= right_polygon)
 
 
+@given(strategies.polygons)
+def test_convex_hull(polygon: Polygon) -> None:
+    assert polygon <= polygon.convex_hull
+
+
+@given(strategies.polygons, strategies.polygons)
+def test_relation_with_lower_than(left_polygon: Polygon,
+                                  right_polygon: Polygon) -> None:
+    assert implication(left_polygon < right_polygon,
+                       left_polygon <= right_polygon)
+
+
 @given(strategies.polygons, strategies.polygons)
 def test_relation_with_greater_than_or_equal(left_polygon: Polygon,
                                              right_polygon: Polygon) -> None:
