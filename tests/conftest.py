06734b6dd50cef80687f78506f1a460413dd66ea
Azat Ibrakov <azatibrakov@gmail.com>
2019-07-29 21:12:59 +0300

    Complete tests configurations

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,10 @@
+from datetime import timedelta
+
 from hypothesis import (HealthCheck,
                         settings)
 
 settings.register_profile('default',
                           max_examples=20,
-                          deadline=None,
+                          deadline=timedelta(seconds=10),
                           suppress_health_check=[HealthCheck.filter_too_much,
                                                  HealthCheck.too_slow])
