6b12df23b5aa00ac67a610661dbfb3a1c719e9c4
Azat Ibrakov <azatibrakov@gmail.com>
2019-07-30 20:55:29 +0300

    Complete strategies

--- tests/angular_tests/angle_tests/strategies.py
+++ tests/angular_tests/angle_tests/strategies.py
@@ -1,6 +1,11 @@
 from hypothesis import strategies
+from lz.functional import pack
 
 from gon.angular import Angle
 from tests.strategies import points
 
-angles = strategies.builds(Angle, points, points, points)
+angles = (strategies.lists(points,
+                           min_size=3,
+                           max_size=3,
+                           unique=True)
+          .map(pack(Angle)))
