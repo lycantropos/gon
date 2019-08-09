ae1273cf9d495948377cb6b5b5a2e7e624ce8528
Azat Ibrakov <azatibrakov@gmail.com>
2019-08-09 04:59:59 +0300

    Complete tests configurations

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,15 @@
+import os
 from datetime import timedelta
 
 from hypothesis import (HealthCheck,
                         settings)
 
+on_travis_ci = os.getenv('CI', False)
+on_azure_pipelines = os.getenv('TF_BUILD', False)
 settings.register_profile('default',
-                          max_examples=20,
+                          max_examples=(25
+                                        if on_travis_ci or on_azure_pipelines
+                                        else settings.default.max_examples),
                           deadline=timedelta(seconds=10),
                           suppress_health_check=[HealthCheck.filter_too_much,
                                                  HealthCheck.too_slow])
