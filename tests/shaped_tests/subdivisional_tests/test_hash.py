8d25e312aaacf0ca4ddfcb5d886d508ef575dddf
Azat Ibrakov <azatibrakov@gmail.com>
2019-09-03 15:15:23 +0300

    Add test for 'QuadEdge.__hash__' magic method

---
+++ tests/shaped_tests/subdivisional_tests/test_hash.py
@@ -0,0 +1,26 @@
+from hypothesis import given
+
+from gon.shaped.subdivisional import QuadEdge
+from tests.utils import equivalence
+from . import strategies
+
+
+@given(strategies.quad_edges)
+def test_basic(quad_edge: QuadEdge) -> None:
+    result = hash(quad_edge)
+
+    assert isinstance(result, int)
+
+
+@given(strategies.quad_edges)
+def test_determinism(quad_edge: QuadEdge) -> None:
+    result = hash(quad_edge)
+
+    assert result == hash(quad_edge)
+
+
+@given(strategies.quad_edges, strategies.quad_edges)
+def test_connection_with_equality(left_quad_edge: QuadEdge,
+                                  right_quad_edge: QuadEdge) -> None:
+    assert equivalence(left_quad_edge == right_quad_edge,
+                       hash(left_quad_edge) == hash(right_quad_edge))
