0b91f4c6d024e55030fe3a16b58058a5ce541ccd
Azat Ibrakov <azatibrakov@gmail.com>
2019-09-03 15:53:14 +0300

    Add test for 'QuadEdge.orientation_with' method

--- tests/shaped_tests/subdivisional_tests/strategies.py
+++ tests/shaped_tests/subdivisional_tests/strategies.py
@@ -6,9 +6,19 @@ from lz.replication import duplicate
 from gon.shaped.subdivisional import QuadEdge
 from tests.strategies import (scalars_strategies,
                               scalars_to_points)
+from tests.utils import triplicate
 
 points_pairs = (scalars_strategies
                 .flatmap(compose(pack(strategies.tuples),
                                  duplicate,
                                  scalars_to_points)))
 quad_edges = points_pairs.map(pack(QuadEdge.factory))
+points_triplets = (scalars_strategies
+                   .flatmap(compose(pack(strategies.tuples),
+                                    triplicate,
+                                    scalars_to_points)))
+quad_edges_with_points = (points_triplets
+                          .map(lambda points_triplet:
+                               (QuadEdge.factory(points_triplet[0],
+                                                 points_triplet[1]),
+                                points_triplet[2])))
