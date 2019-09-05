6b7ae867e4ded2ff0e7074aa9968613eb852d6ab
Azat Ibrakov <azatibrakov@gmail.com>
2019-09-05 18:10:19 +0300

    Complete 'QuadEdge.__slots__' magic field

--- gon/shaped/subdivisional.py
+++ gon/shaped/subdivisional.py
@@ -14,7 +14,7 @@ class QuadEdge:
         https://en.wikipedia.org/wiki/Quad-edge
         http://www.sccg.sk/~samuelcik/dgs/quad_edge.pdf
     """
-    __slots__ = ('_start', '_rotated', '_left_from_start')
+    __slots__ = ('_start', '_left_from_start', '_rotated')
 
     def __init__(self,
                  start: Optional[Point] = None,
