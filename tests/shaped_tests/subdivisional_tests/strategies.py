2a541885ac1e851e3dfbedfc57dca2896a4cb900
Azat Ibrakov <azatibrakov@gmail.com>
2019-09-05 17:30:19 +0300

    Complete strategies

--- tests/shaped_tests/subdivisional_tests/strategies.py
+++ tests/shaped_tests/subdivisional_tests/strategies.py
@@ -1,24 +1,43 @@
+from functools import partial
+from typing import Sequence
+
 from hypothesis import strategies
 from lz.functional import (compose,
                            pack)
 from lz.replication import duplicate
 
+from gon.base import Point
 from gon.shaped.subdivisional import QuadEdge
+from gon.shaped.triangular import _delaunay
 from tests.strategies import (scalars_strategies,
                               scalars_to_points)
-from tests.utils import triplicate
+from tests.utils import (Strategy,
+                         points_do_not_lie_on_the_same_line,
+                         triplicate)
 
 points_pairs = (scalars_strategies
                 .flatmap(compose(pack(strategies.tuples),
                                  duplicate,
                                  scalars_to_points)))
-quad_edges = points_pairs.map(pack(QuadEdge.factory))
+
+
+def points_to_quad_edge(points: Sequence[Point]) -> Strategy[QuadEdge]:
+    triangulation = _delaunay(points)
+    return strategies.sampled_from(list(triangulation.to_edges()))
+
+
+quad_edges = (scalars_strategies
+              .flatmap(compose(partial(strategies.lists,
+                                       min_size=2,
+                                       unique=True),
+                               scalars_to_points))
+              .filter(points_do_not_lie_on_the_same_line)
+              .flatmap(points_to_quad_edge))
 points_triplets = (scalars_strategies
                    .flatmap(compose(pack(strategies.tuples),
                                     triplicate,
                                     scalars_to_points)))
 quad_edges_with_points = (points_triplets
                           .map(lambda points_triplet:
-                               (QuadEdge.factory(points_triplet[0],
-                                                 points_triplet[1]),
+                               (points_to_quad_edge(points_triplet[:2]),
                                 points_triplet[2])))
