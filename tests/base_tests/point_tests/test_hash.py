c89fa51b46de4bdb386af92bce2e4e9efe8f9b02
Azat Ibrakov <azatibrakov@gmail.com>
2019-09-17 21:54:46 +0300

    Add test for 'Point.__hash__' magic method

---
+++ tests/base_tests/point_tests/test_hash.py
@@ -0,0 +1,28 @@
+from hypothesis import (example,
+                        given)
+
+from gon.linear import Point
+from tests.utils import equivalence
+from . import strategies
+
+
+@given(strategies.points)
+def test_basic(point: Point) -> None:
+    result = hash(point)
+
+    assert isinstance(result, int)
+
+
+@given(strategies.points)
+def test_determinism(point: Point) -> None:
+    result = hash(point)
+
+    assert result == hash(point)
+
+
+@given(strategies.points, strategies.points)
+@example(Point(-1.0, -1.0), Point(2.0, 2.0))
+def test_connection_with_equality(left_point: Point,
+                                  right_point: Point) -> None:
+    assert equivalence(left_point == right_point,
+                       hash(left_point) == hash(right_point))
