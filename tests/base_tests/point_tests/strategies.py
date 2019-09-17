f5f3f56d2580e28d4b725eed90458594df669ea1
Azat Ibrakov <azatibrakov@gmail.com>
2019-09-17 21:54:46 +0300

    Add test for 'Point.__hash__' magic method

--- tests/base_tests/point_tests/strategies.py
+++ tests/base_tests/point_tests/strategies.py
@@ -2,7 +2,9 @@ import math
 from hypothesis import strategies
 from lz.functional import identity
 
-from tests.strategies import scalars_strategies
+from tests.strategies import (points,
+                              scalars_strategies)
 
 valid_coordinates = scalars_strategies.flatmap(identity)
 invalid_coordinates = strategies.sampled_from([math.nan, math.inf, -math.inf])
+points = points
