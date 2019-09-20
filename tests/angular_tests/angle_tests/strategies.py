b2d5cac8ba8ff6a810603cdda7bf0ade8e4823bf
Azat Ibrakov <azatibrakov@gmail.com>
2019-09-20 19:40:12 +0300

    Complete strategies

--- tests/angular_tests/angle_tests/strategies.py
+++ tests/angular_tests/angle_tests/strategies.py
@@ -1,11 +1,13 @@
+from functools import partial
+
 from hypothesis import strategies
 from lz.functional import pack
 
 from gon.angular import Angle
-from tests.strategies import points
+from tests.strategies import points_strategies
 
-angles = (strategies.lists(points,
-                           min_size=3,
-                           max_size=3,
-                           unique=True)
-          .map(pack(Angle)))
+unique_points_triplets = points_strategies.flatmap(partial(strategies.lists,
+                                                           min_size=3,
+                                                           max_size=3,
+                                                           unique=True))
+angles = unique_points_triplets.map(pack(Angle))
--- tests/angular_tests/angle_tests/test_creation.py
+++ tests/angular_tests/angle_tests/test_creation.py
@@ -5,10 +5,10 @@ from gon.base import Point
 from . import strategies
 
 
-@given(strategies.points, strategies.points, strategies.points)
-def test_basic(vertex: Point,
-               first_ray_point: Point,
-               second_ray_point: Point) -> None:
+@given(strategies.unique_points_triplets)
+def test_basic(points_triplet: Point) -> None:
+    vertex, first_ray_point, second_ray_point = points_triplet
+
     angle = Angle(first_ray_point, vertex, second_ray_point)
 
     assert angle.vertex == vertex
