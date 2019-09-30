20a270fd45e04844b02741595577316252b10a6d
Ibrakov Azat <azatibrakov@gmail.com>
2019-10-01 05:59:20 +0700

    Complete test for 'Interval.relationship_with' method

--- tests/linear_tests/interval_tests/test_relationship_with.py
+++ tests/linear_tests/interval_tests/test_relationship_with.py
@@ -4,12 +4,22 @@ from hypothesis import given
 
 from gon.linear import (IntersectionKind,
                         Interval)
+from tests.utils import (equivalence,
+                         inverse_inclusion,
+                         is_non_origin_point,
+                         reflect_interval)
 from . import strategies
 
 
 @given(strategies.intervals)
 def test_relationship_with_self(interval: Interval) -> None:
     assert interval.relationship_with(interval) is IntersectionKind.OVERLAP
+
+
+@given(strategies.intervals)
+def test_inversed_inclusion(interval: Interval) -> None:
+    assert (interval.relationship_with(inverse_inclusion(interval))
+            is IntersectionKind.OVERLAP)
 
 
 @given(strategies.intervals_pairs)
@@ -27,3 +37,21 @@ def test_independence_from_ends_order(intervals_pair: Tuple[Interval, Interval]
 
     assert (interval.relationship_with(other_interval)
             is interval.reversed.relationship_with(other_interval))
+
+
+@given(strategies.same_quadrant_intervals)
+def test_same_quadrant_interval_reflection(interval: Interval) -> None:
+    reflected_interval = reflect_interval(interval)
+
+    one_of_endpoints_is_included_origin = (
+            interval.start_inclusive
+            and not is_non_origin_point(interval.start)
+            or interval.end_inclusive
+            and not is_non_origin_point(interval.end))
+
+    assert equivalence(one_of_endpoints_is_included_origin,
+                       interval.relationship_with(reflected_interval)
+                       is IntersectionKind.CROSS)
+    assert equivalence(not one_of_endpoints_is_included_origin,
+                       interval.relationship_with(reflected_interval)
+                       is IntersectionKind.NONE)
