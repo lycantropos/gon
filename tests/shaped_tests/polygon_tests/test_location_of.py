0833ff3ed6cb81499f14befe45eeb7823d63a1ad
Ibrakov Azat <azatibrakov@gmail.com>
2019-10-05 20:13:34 +0700

    Add test for 'Polygon.location_of' method

---
+++ tests/shaped_tests/polygon_tests/test_location_of.py
@@ -0,0 +1,39 @@
+from typing import Tuple
+
+from hypothesis import given
+
+from gon.base import Point
+from gon.shaped.base import (LocationKind,
+                             Polygon,
+                             to_edges)
+from tests.utils import implication
+from . import strategies
+
+
+@given(strategies.polygons)
+def test_vertices(polygon: Polygon) -> None:
+    assert all(polygon.location_of(vertex) is LocationKind.ON_BOUNDARY
+               for vertex in polygon.vertices)
+
+
+@given(strategies.polygons_with_points)
+def test_point_on_edge(polygon_with_point: Tuple[Polygon, Point]) -> None:
+    polygon, point = polygon_with_point
+
+    assert implication(any(point in edge
+                           for edge in to_edges(polygon.vertices)),
+                       polygon.location_of(point) is LocationKind.ON_BOUNDARY)
+
+
+@given(strategies.polygons_with_points)
+def test_convex_hull(polygon_with_point: Tuple[Polygon, Point]) -> None:
+    polygon, point = polygon_with_point
+
+    assert implication(polygon.location_of(point) is LocationKind.INSIDE,
+                       polygon.convex_hull.location_of(point)
+                       is LocationKind.INSIDE)
+    assert implication(polygon.is_convex
+                       and polygon.location_of(point)
+                       is LocationKind.ON_BOUNDARY,
+                       polygon.convex_hull.location_of(point)
+                       is LocationKind.ON_BOUNDARY)
