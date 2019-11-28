9f112c4c303a0d2bd9cc385af2cb74218ee28501
Azat Ibrakov <azatibrakov@gmail.com>
2019-11-28 13:13:22 +0300

    Complete strategies

--- tests/strategies/base.py
+++ tests/strategies/base.py
@@ -16,11 +16,11 @@ def to_decimals(*,
                 max_value: Optional[Scalar] = None,
                 allow_nan: bool = False,
                 allow_infinity: bool = False) -> Strategy:
-    return (strategies.floats(min_value=min_value,
-                              max_value=max_value,
-                              allow_nan=allow_nan,
-                              allow_infinity=allow_infinity)
-            .map(to_recoverable_significant_digits_count))
+    return (strategies.decimals(min_value=min_value,
+                                max_value=max_value,
+                                allow_nan=allow_nan,
+                                allow_infinity=allow_infinity)
+            .map(to_digits_count))
 
 
 def to_floats(*,
@@ -32,7 +32,7 @@ def to_floats(*,
                               max_value=max_value,
                               allow_nan=allow_nan,
                               allow_infinity=allow_infinity)
-            .map(to_recoverable_significant_digits_count))
+            .map(to_digits_count))
 
 
 def to_fractions(*,
@@ -42,7 +42,7 @@ def to_fractions(*,
     return (strategies.fractions(min_value=min_value,
                                  max_value=max_value,
                                  max_denominator=max_denominator)
-            .map(to_recoverable_significant_digits_count))
+            .map(to_digits_count))
 
 
 def to_integers(*,
@@ -50,30 +50,39 @@ def to_integers(*,
                 max_value: Optional[Scalar] = None) -> Strategy:
     return (strategies.integers(min_value=min_value,
                                 max_value=max_value)
-            .map(to_recoverable_significant_digits_count))
+            .map(to_digits_count))
 
 
-def to_recoverable_significant_digits_count(
-        number: SupportsFloat,
-        *,
-        max_digits_count: int = sys.float_info.dig) -> SupportsFloat:
-    decimal = to_decimal(number)
-    sign, digits, exponent = decimal.as_tuple()
-    if len(digits) <= max_digits_count:
+def to_digits_count(number: Scalar,
+                    *,
+                    max_digits_count: int = sys.float_info.dig) -> Scalar:
+    decimal = to_decimal(number).normalize()
+    _, significant_digits, exponent = decimal.as_tuple()
+    significant_digits_count = len(significant_digits)
+    if exponent < 0:
+        fixed_digits_count = (1 - exponent
+                              if exponent <= -significant_digits_count
+                              else significant_digits_count)
+    else:
+        fixed_digits_count = exponent + significant_digits_count
+    if fixed_digits_count <= max_digits_count:
         return number
-    whole_digits_count = len(digits) + exponent
-    whole_digits_offset = max(whole_digits_count - max_digits_count, 0)
-    decimal /= 10 ** whole_digits_offset
-    decimal_digits_count = -exponent + whole_digits_offset
-    decimal_digits_limit = max(min(max_digits_count, decimal_digits_count)
-                               - whole_digits_offset,
-                               0)
-    decimal = round(decimal, decimal_digits_limit)
+    whole_digits_count = max(significant_digits_count + exponent, 0)
+    if whole_digits_count:
+        whole_digits_offset = max(whole_digits_count - max_digits_count, 0)
+        decimal /= 10 ** whole_digits_offset
+        whole_digits_count -= whole_digits_offset
+    else:
+        decimal *= 10 ** (-exponent - significant_digits_count)
+        whole_digits_count = 1
+    decimal = round(decimal, max(max_digits_count - whole_digits_count, 0))
     return type(number)(str(decimal))
 
 
 def to_decimal(number: SupportsFloat) -> Decimal:
-    if not isinstance(number, (int, float)):
+    if isinstance(number, Decimal):
+        return number
+    elif not isinstance(number, (int, float)):
         number = float(number)
     return Decimal(number)
 
