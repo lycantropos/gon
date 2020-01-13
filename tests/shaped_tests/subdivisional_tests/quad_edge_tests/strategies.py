2b861cda5b331184919b270114a6ddaaecc7c148
Azat Ibrakov <azatibrakov@gmail.com>
2020-01-13 18:30:42 +0300

    Complete strategies

--- tests/shaped_tests/subdivisional_tests/quad_edge_tests/strategies.py
+++ tests/shaped_tests/subdivisional_tests/quad_edge_tests/strategies.py
@@ -1,19 +1,24 @@
-from functools import partial
-from typing import Sequence
+from typing import (List,
+                    Optional)
 
 from hypothesis import strategies
 from lz.functional import (compose,
+                           identity,
                            pack)
 from lz.replication import duplicate
 
 from gon.base import Point
+from gon.hints import Scalar
 from gon.shaped import triangular
 from gon.shaped.subdivisional import QuadEdge
+from gon.shaped.triangular import Triangulation
 from tests.strategies import (scalars_strategies,
                               scalars_to_points)
 from tests.utils import (Strategy,
+                         cleave_in_tuples,
                          points_do_not_lie_on_the_same_line,
-                         triplicate)
+                         to_pairs,
+                         to_triplets)
 
 points_pairs = (scalars_strategies
                 .flatmap(compose(pack(strategies.tuples),
@@ -21,39 +26,45 @@ points_pairs = (scalars_strategies
                                  scalars_to_points)))
 
 
-def points_to_quad_edge(points: Sequence[Point]) -> Strategy[QuadEdge]:
-    triangulation = triangular.delaunay(points)
-    return strategies.sampled_from(list(triangulation.to_edges()))
+def scalars_to_quad_edges(scalars: Strategy[Scalar]) -> Strategy[QuadEdge]:
+    return (scalars_to_points_in_general_position(scalars)
+            .map(triangular.delaunay)
+            .map(Triangulation.to_edges)
+            .map(list)
+            .flatmap(strategies.sampled_from))
 
 
-quad_edges = (scalars_strategies
-              .flatmap(compose(partial(strategies.lists,
-                                       min_size=2,
-                                       unique=True),
-                               scalars_to_points))
-              .filter(points_do_not_lie_on_the_same_line)
-              .flatmap(points_to_quad_edge))
+def scalars_to_points_in_general_position(scalars: Strategy[Scalar],
+                                          *,
+                                          min_size: int = 2,
+                                          max_size: Optional[int] = None
+                                          ) -> Strategy[List[Point]]:
+    return (strategies.lists(scalars_to_points(scalars),
+                             min_size=min_size,
+                             max_size=max_size,
+                             unique=True)
+            .filter(points_do_not_lie_on_the_same_line))
 
 
-def points_to_quad_edge_with_neighbours(points: Sequence[Point]
-                                        ) -> Strategy[QuadEdge]:
-    triangulation = triangular.delaunay(points)
-    return strategies.sampled_from(list(triangulation.to_inner_edges()))
+quad_edges_strategies = scalars_strategies.map(scalars_to_quad_edges)
+quad_edges = quad_edges_strategies.flatmap(identity)
+quad_edges_pairs = quad_edges_strategies.flatmap(to_pairs)
+quad_edges_triplets = quad_edges_strategies.flatmap(to_triplets)
+
+
+def scalars_to_quad_edges_with_neighbours(scalars: Strategy[Scalar]
+                                          ) -> Strategy[QuadEdge]:
+    return (scalars_to_points_in_general_position(scalars,
+                                                  min_size=4)
+            .map(triangular.delaunay)
+            .map(Triangulation.to_inner_edges)
+            .map(list)
+            .map(strategies.sampled_from))
 
 
 quad_edges_with_neighbours = (scalars_strategies
-                              .flatmap(compose(partial(strategies.lists,
-                                                       min_size=4,
-                                                       unique=True),
-                                               scalars_to_points))
-                              .filter(points_do_not_lie_on_the_same_line)
-                              .flatmap(points_to_quad_edge_with_neighbours))
-points_triplets = (scalars_strategies
-                   .flatmap(compose(pack(strategies.tuples),
-                                    triplicate,
-                                    scalars_to_points)))
-quad_edges_with_points = (points_triplets
-                          .map(lambda points_triplet:
-                               (QuadEdge.factory(points_triplet[0],
-                                                 points_triplet[1]),
-                                points_triplet[2])))
+                              .flatmap(scalars_to_quad_edges_with_neighbours))
+quad_edges_with_points = (scalars_strategies
+                          .flatmap(cleave_in_tuples(scalars_to_quad_edges,
+                                                    scalars_to_points)))
+non_quad_edges = strategies.builds(object)
