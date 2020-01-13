d2c38ffec4354b49d5960e7e8415c4c053180265
Azat Ibrakov <azatibrakov@gmail.com>
2020-01-13 19:41:37 +0300

    Complete strategies

--- tests/shaped_tests/contracts_tests/strategies.py
+++
@@ -1,4 +0,0 @@
-from tests.strategies import (scalars_strategies,
-                              scalars_to_triangles_vertices)
-
-triangles_vertices = scalars_strategies.flatmap(scalars_to_triangles_vertices)
--- tests/strategies/__init__.py
+++ tests/strategies/__init__.py
@@ -3,4 +3,5 @@ from .base import (points,
                    scalars_strategies,
                    scalars_to_points)
 from .shaped import (scalars_to_triangles_vertices,
-                     to_non_triangle_vertices_base)
+                     to_non_triangle_vertices_base,
+                     triangles_vertices)
