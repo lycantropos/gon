213a7118120179a86071b004747000ee0e071bfe
Azat Ibrakov <azatibrakov@gmail.com>
2020-01-13 18:45:28 +0300

    Complete test for 'QuadEdge.__hash__' magic method

--- tests/shaped_tests/subdivisional_tests/quad_edge_tests/test_hash.py
+++ tests/shaped_tests/subdivisional_tests/quad_edge_tests/test_hash.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 from hypothesis import given
 
 from gon.shaped.subdivisional import QuadEdge
@@ -19,8 +21,10 @@ def test_determinism(quad_edge: QuadEdge) -> None:
     assert result == hash(quad_edge)
 
 
-@given(strategies.quad_edges, strategies.quad_edges)
-def test_connection_with_equality(left_quad_edge: QuadEdge,
-                                  right_quad_edge: QuadEdge) -> None:
+@given(strategies.quad_edges_pairs)
+def test_connection_with_equality(quad_edges_pair: Tuple[QuadEdge, QuadEdge]
+                                  ) -> None:
+    left_quad_edge, right_quad_edge = quad_edges_pair
+
     assert implication(left_quad_edge == right_quad_edge,
                        hash(left_quad_edge) == hash(right_quad_edge))
