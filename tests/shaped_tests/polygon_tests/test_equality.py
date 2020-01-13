7ea2bb1b322f2c5134a112f4963e4e4ba1fa6b14
Azat Ibrakov <azatibrakov@gmail.com>
2020-01-13 17:39:17 +0300

    Complete test for 'Polygon.__eq__' magic method

--- tests/shaped_tests/polygon_tests/test_equality.py
+++ tests/shaped_tests/polygon_tests/test_equality.py
@@ -14,16 +14,19 @@ def test_reflexivity(polygon: Polygon) -> None:
     assert polygon == polygon
 
 
-@given(strategies.polygons, strategies.polygons)
-def test_symmetry(left_polygon: Polygon, right_polygon: Polygon) -> None:
+@given(strategies.polygons_pairs)
+def test_symmetry(polygons_pair: Tuple[Polygon, Polygon]) -> None:
+    left_polygon, right_polygon = polygons_pair
+
     assert implication(left_polygon == right_polygon,
                        right_polygon == left_polygon)
 
 
-@given(strategies.polygons, strategies.polygons, strategies.polygons)
-def test_transitivity(left_polygon: Polygon,
-                      mid_polygon: Polygon,
-                      right_polygon: Polygon) -> None:
+@given(strategies.polygons_triplets)
+def test_transitivity(polygons_triplet: Tuple[Polygon, Polygon, Polygon]
+                      ) -> None:
+    left_polygon, mid_polygon, right_polygon = polygons_triplet
+
     assert implication(left_polygon == mid_polygon == right_polygon,
                        left_polygon == right_polygon)
 
