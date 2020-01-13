1cff416d6de0dc5a012ea4c577dd9faadb0ab458
Azat Ibrakov <azatibrakov@gmail.com>
2020-01-13 18:44:39 +0300

    Complete test for 'QuadEdge.__eq__' magic method

--- tests/shaped_tests/subdivisional_tests/quad_edge_tests/test_equality.py
+++ tests/shaped_tests/subdivisional_tests/quad_edge_tests/test_equality.py
@@ -1,3 +1,6 @@
+from typing import (Any,
+                    Tuple)
+
 from hypothesis import given
 
 from gon.shaped.subdivisional import QuadEdge
@@ -10,20 +13,24 @@ def test_reflexivity(quad_edge: QuadEdge) -> None:
     assert quad_edge == quad_edge
 
 
-@given(strategies.quad_edges, strategies.quad_edges)
-def test_symmetry(left_quad_edge: QuadEdge, right_quad_edge: QuadEdge) -> None:
+@given(strategies.quad_edges_pairs)
+def test_symmetry(quad_edges_pair: Tuple[QuadEdge, QuadEdge]) -> None:
+    left_quad_edge, right_quad_edge = quad_edges_pair
+
     assert implication(left_quad_edge == right_quad_edge,
                        right_quad_edge == left_quad_edge)
 
 
-@given(strategies.quad_edges, strategies.quad_edges, strategies.quad_edges)
-def test_transitivity(left_quad_edge: QuadEdge,
-                      mid_quad_edge: QuadEdge,
-                      right_quad_edge: QuadEdge) -> None:
+@given(strategies.quad_edges_triplets)
+def test_transitivity(quad_edges_triplet: Tuple[QuadEdge, QuadEdge, QuadEdge]
+                      ) -> None:
+    left_quad_edge, mid_quad_edge, right_quad_edge = quad_edges_triplet
+
     assert implication(left_quad_edge == mid_quad_edge == right_quad_edge,
                        left_quad_edge == right_quad_edge)
 
 
-@given(strategies.quad_edges)
-def test_non_quad_edge_argument(quad_edge: QuadEdge) -> None:
-    assert quad_edge != object()
+@given(strategies.quad_edges, strategies.non_quad_edges)
+def test_non_quad_edge_argument(quad_edge: QuadEdge,
+                                non_quad_edge: Any) -> None:
+    assert quad_edge != non_quad_edge
