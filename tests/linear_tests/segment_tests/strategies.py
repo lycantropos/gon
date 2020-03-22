500fc2b7a8fefcae3d0716b8170a5d5ff2a46016
Georgy <skorobogatov@phystech.edu>
2020-03-22 19:54:06 +0100

    Replace 'scalar' with 'coordinate' in names of variables

--- tests/linear_tests/segment_tests/strategies.py
+++ tests/linear_tests/segment_tests/strategies.py
@@ -8,25 +8,25 @@ from lz.functional import (compose,
 from lz.replication import duplicate
 
 from gon.linear import Segment
-from tests.strategies import (scalars_strategies,
-                              scalars_to_points)
+from tests.strategies import (coordinates_strategies,
+                              coordinates_to_points)
 from tests.utils import (Strategy,
                          cleave_in_tuples,
                          to_pairs,
                          to_triplets)
 
-scalars_to_segments = compose(methodcaller(Strategy.map.__name__,
-                                           pack(Segment)),
-                              methodcaller(Strategy.filter.__name__,
-                                           pack(ne)),
-                              pack(strategies.tuples),
-                              duplicate,
-                              scalars_to_points)
-segments_strategies = scalars_strategies.map(scalars_to_segments)
+coordinates_to_segments = compose(methodcaller(Strategy.map.__name__,
+                                               pack(Segment)),
+                                  methodcaller(Strategy.filter.__name__,
+                                               pack(ne)),
+                                  pack(strategies.tuples),
+                                  duplicate,
+                                  coordinates_to_points)
+segments_strategies = coordinates_strategies.map(coordinates_to_segments)
 segments = segments_strategies.flatmap(identity)
-segments_with_points = (scalars_strategies
-                        .flatmap(cleave_in_tuples(scalars_to_segments,
-                                                  scalars_to_points)))
+segments_with_points = (coordinates_strategies
+                        .flatmap(cleave_in_tuples(coordinates_to_segments,
+                                                  coordinates_to_points)))
 segments_pairs = segments_strategies.flatmap(to_pairs)
 segments_triplets = segments_strategies.flatmap(to_triplets)
 non_segments = strategies.builds(object)
