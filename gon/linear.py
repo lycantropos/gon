b2586eecadc881ba1b43b2e10a95c9fbc85af04c
Azat Ibrakov <azatibrakov@gmail.com>
2020-03-24 14:56:52 +0300

    Complete 'Segment.relationship_with' method

--- gon/linear.py
+++ gon/linear.py
@@ -3,11 +3,11 @@ from typing import (Tuple,
 
 from reprit.base import generate_repr
 from robust.linear import (SegmentsRelationship,
-                           segment_contains)
+                           segment_contains,
+                           segments_relationship)
 
 from gon.hints import Coordinate
 from .angular import (Angle,
-                      AngleKind,
                       Orientation)
 from .base import Point
 
@@ -54,93 +54,7 @@ class Segment:
         return segment_contains(self.as_tuple(), point.as_tuple())
 
     def relationship_with(self, other: 'Segment') -> SegmentsRelationship:
-        if self == other:
-            return SegmentsRelationship.OVERLAP
-        self_start_orientation = other.orientation_with(self.start)
-        self_end_orientation = other.orientation_with(self.end)
-        if (self_start_orientation is Orientation.COLLINEAR
-                and _in_segment(self.start, other)):
-            if self_end_orientation is Orientation.COLLINEAR:
-                if self.start == other.start:
-                    if (Angle(self.end, self.start, other.end).kind
-                            is AngleKind.ACUTE):
-                        return SegmentsRelationship.OVERLAP
-                    else:
-                        return SegmentsRelationship.CROSS
-                elif self.start == other.end:
-                    if (Angle(self.end, self.start, other.start).kind
-                            is AngleKind.ACUTE):
-                        return SegmentsRelationship.OVERLAP
-                    else:
-                        return SegmentsRelationship.CROSS
-                else:
-                    return SegmentsRelationship.OVERLAP
-            else:
-                return SegmentsRelationship.CROSS
-        elif (self_end_orientation is Orientation.COLLINEAR
-              and _in_segment(self.end, other)):
-            if self_start_orientation is Orientation.COLLINEAR:
-                if self.end == other.start:
-                    if (Angle(self.start, self.end, other.end).kind
-                            is AngleKind.ACUTE):
-                        return SegmentsRelationship.OVERLAP
-                    else:
-                        return SegmentsRelationship.CROSS
-                elif self.end == other.end:
-                    if (Angle(self.start, self.end, other.start).kind
-                            is AngleKind.ACUTE):
-                        return SegmentsRelationship.OVERLAP
-                    else:
-                        return SegmentsRelationship.CROSS
-                else:
-                    return SegmentsRelationship.OVERLAP
-            else:
-                return SegmentsRelationship.CROSS
-        other_start_orientation = self.orientation_with(other.start)
-        other_end_orientation = self.orientation_with(other.end)
-        if (self_start_orientation * self_end_orientation < 0
-                and other_start_orientation * other_end_orientation < 0):
-            return SegmentsRelationship.CROSS
-        elif (other_start_orientation is Orientation.COLLINEAR
-              and _in_segment(other.start, self)):
-            if other_end_orientation is Orientation.COLLINEAR:
-                if other.start == self.start:
-                    if (Angle(other.end, other.start, self.end).kind
-                            is AngleKind.ACUTE):
-                        return SegmentsRelationship.OVERLAP
-                    else:
-                        return SegmentsRelationship.CROSS
-                elif other.start == self.end:
-                    if (Angle(other.end, other.start, self.start).kind
-                            is AngleKind.ACUTE):
-                        return SegmentsRelationship.OVERLAP
-                    else:
-                        return SegmentsRelationship.CROSS
-                else:
-                    return SegmentsRelationship.OVERLAP
-            else:
-                return SegmentsRelationship.CROSS
-        elif (other_end_orientation is Orientation.COLLINEAR
-              and _in_segment(other.end, self)):
-            if other_start_orientation is Orientation.COLLINEAR:
-                if other.end == self.start:
-                    if (Angle(other.start, other.end, self.end).kind
-                            is AngleKind.ACUTE):
-                        return SegmentsRelationship.OVERLAP
-                    else:
-                        return SegmentsRelationship.CROSS
-                elif other.end == self.end:
-                    if (Angle(other.start, other.end, self.start).kind
-                            is AngleKind.ACUTE):
-                        return SegmentsRelationship.OVERLAP
-                    else:
-                        return SegmentsRelationship.CROSS
-                else:
-                    return SegmentsRelationship.OVERLAP
-            else:
-                return SegmentsRelationship.CROSS
-        else:
-            return SegmentsRelationship.NONE
+        return segments_relationship(self.as_tuple(), other.as_tuple())
 
     def orientation_with(self, point: Point) -> Orientation:
         return self.angle_with(point).orientation
