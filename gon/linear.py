3c1dfac8ddc4e1a744bd91026fabdf6c0cc44334
Azat Ibrakov <azatibrakov@gmail.com>
2020-04-04 15:04:16 +0300

    Remove 'Segment.orientation_with' method

--- gon/linear.py
+++ gon/linear.py
@@ -39,6 +39,18 @@ class Segment(Geometry):
 
     __repr__ = generate_repr(__init__)
 
+    def __contains__(self, point: Point) -> bool:
+        return segment_contains(self._raw, point.raw())
+
+    def __eq__(self, other: 'Segment') -> bool:
+        return (self._start == other._start and self._end == other._end
+                or self._start == other._end and self._end == other._start
+                if isinstance(other, Segment)
+                else NotImplemented)
+
+    def __hash__(self) -> int:
+        return hash(frozenset(self._raw))
+
     @property
     def start(self) -> Point:
         return self._start
@@ -56,29 +68,14 @@ class Segment(Geometry):
         start, end = Point.from_raw(raw_start), Point.from_raw(raw_end)
         return cls(start, end)
 
+    def relationship_with(self, other: 'Segment') -> SegmentsRelationship:
+        return segments_relationship(self._raw, other._raw)
+
     def validate(self) -> None:
         self._start.validate()
         self._end.validate()
         if self._start == self._end:
             raise ValueError('Segment is degenerate.')
-
-    def __eq__(self, other: 'Segment') -> bool:
-        return (self._start == other._start and self._end == other._end
-                or self._start == other._end and self._end == other._start
-                if isinstance(other, Segment)
-                else NotImplemented)
-
-    def __hash__(self) -> int:
-        return hash(frozenset(self._raw))
-
-    def __contains__(self, point: Point) -> bool:
-        return segment_contains(self._raw, point.raw())
-
-    def relationship_with(self, other: 'Segment') -> SegmentsRelationship:
-        return segments_relationship(self._raw, other._raw)
-
-    def orientation_with(self, point: Point) -> Orientation:
-        return to_orientation(self._end, self._start, point)
 
 
 class Contour(Geometry):
--- tests/linear_tests/segment_tests/test_contains.py
+++ tests/linear_tests/segment_tests/test_contains.py
@@ -1,11 +1,6 @@
-from typing import Tuple
-
 from hypothesis import given
 
-from gon.angular import Orientation
 from gon.linear import Segment
-from gon.primitive import Point
-from tests.utils import implication
 from . import strategies
 
 
@@ -13,12 +8,3 @@ from . import strategies
 def test_endpoints(segment: Segment) -> None:
     assert segment.start in segment
     assert segment.end in segment
-
-
-@given(strategies.segments_with_points)
-def test_orientation(segment_with_point: Tuple[Segment, Point]) -> None:
-    segment, point = segment_with_point
-
-    assert implication(point in segment,
-                       segment.orientation_with(point)
-                       is Orientation.COLLINEAR)
