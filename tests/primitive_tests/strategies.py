033690613194b0146d2f5549798d5a3148244ae4
Azat Ibrakov <azatibrakov@gmail.com>
2020-04-04 10:28:40 +0300

    Complete test for 'Point.validate' method

--- tests/primitive_tests/strategies.py
+++ tests/primitive_tests/strategies.py
@@ -1,16 +1,10 @@
-import math
-
-from hypothesis import strategies
-from lz.functional import identity
-
-from tests.strategies import (coordinates_strategies,
+from tests.strategies import (invalid_points,
                               points,
                               points_strategies)
 from tests.utils import (to_pairs,
                          to_triplets)
 
-valid_coordinates = coordinates_strategies.flatmap(identity)
-invalid_coordinates = strategies.sampled_from([math.nan, math.inf, -math.inf])
+invalid_points = invalid_points
 points = points
 points_pairs = points_strategies.flatmap(to_pairs)
 points_triplets = points_strategies.flatmap(to_triplets)
