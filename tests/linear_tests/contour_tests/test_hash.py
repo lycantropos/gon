d046bf04c913a5371a14cd7fb717a09da97ce0ae
Azat Ibrakov <azatibrakov@gmail.com>
2020-04-04 07:05:14 +0300

    Add test for 'Contour.__hash__' magic method

---
+++ tests/linear_tests/contour_tests/test_hash.py
@@ -0,0 +1,23 @@
+from typing import Tuple
+
+from hypothesis import given
+
+from gon.linear import Contour
+from tests.utils import implication
+from . import strategies
+
+
+@given(strategies.contours)
+def test_determinism(contour: Contour) -> None:
+    result = hash(contour)
+
+    assert result == hash(contour)
+
+
+@given(strategies.contours_pairs)
+def test_connection_with_equality(contours_pair: Tuple[Contour, Contour]
+                                  ) -> None:
+    left_contour, right_contour = contours_pair
+
+    assert implication(left_contour == right_contour,
+                       hash(left_contour) == hash(right_contour))
