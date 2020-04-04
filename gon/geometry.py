e45d0e12983a12c043145daaaabcf689f9f94d5e
Azat Ibrakov <azatibrakov@gmail.com>
2020-04-04 14:53:54 +0300

    Complete order of members for 'Geometry' class

--- gon/geometry.py
+++ gon/geometry.py
@@ -10,9 +10,21 @@ RawGeometry = TypeVar('RawGeometry', tuple, list)
 
 class Geometry(ABC):
     @abstractmethod
-    def raw(self) -> RawGeometry:
+    def __eq__(self, other: 'Geometry') -> bool:
         """
-        Returns geometric object as combination of Python built-ins.
+        Checks if geometric objects are equal.
+        """
+
+    @abstractmethod
+    def __hash__(self) -> int:
+        """
+        Returns hash of the geometric object.
+        """
+
+    @abstractmethod
+    def __repr__(self) -> str:
+        """
+        Returns string representation of the geometric object.
         """
 
     @classmethod
@@ -23,26 +35,14 @@ class Geometry(ABC):
         """
 
     @abstractmethod
+    def raw(self) -> RawGeometry:
+        """
+        Returns geometric object as combination of Python built-ins.
+        """
+
+    @abstractmethod
     def validate(self) -> None:
         """
         Checks geometric object's constraints
         and raises error if any violation was found.
-        """
-
-    @abstractmethod
-    def __repr__(self) -> str:
-        """
-        Returns string representation of the geometric object.
-        """
-
-    @abstractmethod
-    def __hash__(self) -> int:
-        """
-        Returns hash of the geometric object.
-        """
-
-    @abstractmethod
-    def __eq__(self, other: 'Geometry') -> bool:
-        """
-        Checks if geometric objects are equal.
         """
