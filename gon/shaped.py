93932d48b7bc46a3e80c6b553ed35a34b46bc8ea
Azat Ibrakov <azatibrakov@gmail.com>
2020-04-04 19:36:19 +0300

    Complete docstring for 'Polygon.area' property

--- gon/shaped.py
+++ gon/shaped.py
@@ -88,6 +88,14 @@ class Polygon(Geometry):
         """
         Returns area of the polygon.
 
+        Time complexity:
+            ``O(vertices_count)``
+        Memory complexity:
+            ``O(1)``
+
+        where ``vertices_count = len(self.border.vertices)\
+ + sum(len(hole.vertices) for hole in self.holes)``.
+
         >>> polygon = Polygon.from_raw(([(0, 0), (6, 0), (6, 6), (0, 6)],
         ...                             [[(2, 2), (2, 4), (4, 4), (4, 2)]]))
         >>> polygon.area == 32
