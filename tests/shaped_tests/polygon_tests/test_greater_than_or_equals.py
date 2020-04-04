01d155c4fe8ffc57fd3d11f0be25571ee17fc055
Azat Ibrakov <azatibrakov@gmail.com>
2020-04-04 23:45:24 +0300

    Add test for 'Polygon.__ge__' magic method

---
+++ tests/shaped_tests/polygon_tests/test_greater_than_or_equals.py
@@ -0,0 +1,30 @@
+from typing import Tuple
+
+from hypothesis import given
+
+from gon.shaped import Polygon
+from tests.utils import (equivalence,
+                         implication)
+from . import strategies
+
+
+@given(strategies.polygons)
+def test_reflexivity(polygon: Polygon) -> None:
+    assert polygon >= polygon
+
+
+@given(strategies.polygons_pairs)
+def test_antisymmetry(polygons_pair: Tuple[Polygon, Polygon]) -> None:
+    first_polygon, second_polygon = polygons_pair
+
+    assert equivalence(first_polygon >= second_polygon >= first_polygon,
+                       first_polygon == second_polygon)
+
+
+@given(strategies.polygons_triplets)
+def test_transitivity(polygons_triplet: Tuple[Polygon, Polygon, Polygon]
+                      ) -> None:
+    first_polygon, second_polygon, third_polygon = polygons_triplet
+
+    assert implication(first_polygon >= second_polygon >= third_polygon,
+                       first_polygon >= third_polygon)
