71eef022a6b8d09a237641257b0a105b0fc798ac
Azat Ibrakov <azatibrakov@gmail.com>
2020-04-04 23:47:04 +0300

    Complete test for 'Polygon.__lt__' magic method

--- tests/shaped_tests/polygon_tests/test_lower_than.py
+++ tests/shaped_tests/polygon_tests/test_lower_than.py
@@ -3,7 +3,8 @@ from typing import Tuple
 from hypothesis import given
 
 from gon.shaped import Polygon
-from tests.utils import implication
+from tests.utils import (equivalence,
+                         implication)
 from . import strategies
 
 
@@ -27,3 +28,12 @@ def test_transitivity(polygons_triplet: Tuple[Polygon, Polygon, Polygon]
 
     assert implication(first_polygon < second_polygon < third_polygon,
                        first_polygon < third_polygon)
+
+
+@given(strategies.polygons_pairs)
+def test_connection_with_greater_than(
+        polygons_pair: Tuple[Polygon, Polygon]) -> None:
+    first_polygon, second_polygon = polygons_pair
+
+    assert equivalence(first_polygon < second_polygon,
+                       second_polygon > first_polygon)
