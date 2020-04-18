d9c211abb77ca11039ee79d6c600306bf5c1c356
Azat Ibrakov <azatibrakov@gmail.com>
2020-04-18 18:19:59 +0300

    Complete tests for 'geometry' module

---
+++ tests/geometry_tests/compounds_tests/test_relate.py
@@ -0,0 +1,30 @@
+from typing import Tuple
+
+from hypothesis import given
+from orient.planar import Relation
+
+from gon.geometry import Compound
+from tests.geometry_tests.compounds_tests import strategies
+
+
+@given(strategies.compounds_pairs)
+def test_basic(compounds_pair: Tuple[Compound, Compound]) -> None:
+    left_compound, right_compound = compounds_pair
+
+    result = left_compound.relate(right_compound)
+
+    assert isinstance(result, Relation)
+
+
+@given(strategies.compounds)
+def test_self(compound: Compound) -> None:
+    assert compound.relate(compound) is Relation.EQUAL
+
+
+@given(strategies.compounds_pairs)
+def test_complement(compounds_pair: Tuple[Compound, Compound]) -> None:
+    left_compound, right_compound = compounds_pair
+
+    result = left_compound.relate(right_compound)
+
+    assert result.complement is right_compound.relate(left_compound)
