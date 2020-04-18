608d97620ec7d68588a7bc231a476ed40aae820a
Azat Ibrakov <azatibrakov@gmail.com>
2020-04-18 23:34:24 +0300

    Add tests for 'Contour' class

--- tests/strategies/factories.py
+++ tests/strategies/factories.py
@@ -4,7 +4,8 @@ from hypothesis import strategies
 from hypothesis_geometry import planar
 
 from gon.hints import Coordinate
-from gon.linear import (Loop,
+from gon.linear import (Contour,
+                        Loop,
                         Segment)
 from gon.primitive import Point
 from gon.shaped import Polygon
@@ -19,6 +20,17 @@ def coordinates_to_points(coordinates: Strategy[Coordinate]
 def coordinates_to_segments(coordinates: Strategy[Coordinate]
                             ) -> Strategy[Segment]:
     return planar.segments(coordinates).map(Segment.from_raw)
+
+
+def coordinates_to_contours(coordinates: Strategy[Coordinate],
+                            *,
+                            min_size: int = planar.TRIANGULAR_CONTOUR_SIZE,
+                            max_size: Optional[int] = None
+                            ) -> Strategy[Contour]:
+    return (planar.contours(coordinates,
+                            min_size=min_size,
+                            max_size=max_size)
+            .map(Contour.from_raw))
 
 
 def coordinates_to_loops(coordinates: Strategy[Coordinate],
--- tests/strategies/linear.py
+++ tests/strategies/linear.py
@@ -1,33 +1,35 @@
 from functools import partial
 from operator import (itemgetter,
                       mul)
-from typing import Optional
+from typing import (List,
+                    Optional)
 
 from hypothesis import strategies
 from hypothesis_geometry import planar
 
 from gon.hints import Coordinate
 from gon.linear import (MIN_VERTICES_COUNT,
+                        Contour,
                         Loop,
                         Vertices)
+from gon.primitive import Point
 from tests.utils import Strategy
 from .base import coordinates_strategies
 from .factories import coordinates_to_points
 from .primitive import invalid_points
 
 
-def to_loops_with_repeated_points(coordinates: Strategy[Coordinate],
-                                  min_size: int = MIN_VERTICES_COUNT,
-                                  max_size: Optional[int] = None
-                                  ) -> Strategy[Loop]:
+def to_repeated_points(coordinates: Strategy[Coordinate],
+                       min_size: int = MIN_VERTICES_COUNT,
+                       max_size: Optional[int] = None
+                       ) -> Strategy[List[Point]]:
     return (strategies.lists(planar.points(coordinates),
                              min_size=min_size,
                              max_size=max_size)
             .map(partial(mul, 2))
             .flatmap(strategies.permutations)
             .map(itemgetter(slice(max_size)))
-            .filter(not_all_unique)
-            .map(Loop.from_raw))
+            .filter(not_all_unique))
 
 
 def not_all_unique(vertices: Vertices) -> bool:
@@ -41,15 +43,29 @@ def not_all_unique(vertices: Vertices) -> bool:
     return False
 
 
+small_contours = (
+    strategies.builds(Contour,
+                      coordinates_strategies
+                      .map(coordinates_to_points)
+                      .flatmap(partial(strategies.lists,
+                                       max_size=MIN_VERTICES_COUNT - 1))))
 small_loops = (
     strategies.builds(Loop,
                       coordinates_strategies
                       .map(coordinates_to_points)
                       .flatmap(partial(strategies.lists,
                                        max_size=MIN_VERTICES_COUNT - 1))))
+invalid_vertices_contours = strategies.builds(
+        Contour,
+        strategies.lists(invalid_points,
+                         min_size=MIN_VERTICES_COUNT))
 invalid_vertices_loops = strategies.builds(
         Loop,
         strategies.lists(invalid_points,
                          min_size=MIN_VERTICES_COUNT))
+contours_with_repeated_points = (coordinates_strategies
+                                 .flatmap(to_repeated_points)
+                                 .map(Contour.from_raw))
 loops_with_repeated_points = (coordinates_strategies
-                              .flatmap(to_loops_with_repeated_points))
+                              .flatmap(to_repeated_points)
+                              .map(Loop.from_raw))
