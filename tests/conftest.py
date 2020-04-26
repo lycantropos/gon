c8b101e1066142fbc68289f4dcb3afb2e161c5cf
Azat Ibrakov <azatibrakov@gmail.com>
2020-04-27 00:17:33 +0300

    Complete tests configurations

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,15 @@
 import os
+import platform
 
 from hypothesis import (HealthCheck,
                         settings)
 
 on_travis_ci = os.getenv('CI', False)
 on_azure_pipelines = os.getenv('TF_BUILD', False)
+is_pypy = platform.python_implementation() == 'PyPy'
 settings.register_profile('default',
-                          max_examples=(settings.default.max_examples // 4
+                          max_examples=(settings.default.max_examples
+                                        // (4 * (1 + is_pypy))
                                         if on_travis_ci or on_azure_pipelines
                                         else settings.default.max_examples),
                           deadline=None,
