ad24966165bc186571d30d9eff0ccee9decac8a0
Azat Ibrakov <azatibrakov@gmail.com>
2020-05-18 21:19:27 +0300

    Add test for 'Contour.__contains__' magic method

---
+++ tests/linear_tests/contour_tests/test_contains.py
@@ -0,0 +1,27 @@
+from typing import Tuple
+
+from hypothesis import given
+
+from gon.primitive import Point
+from gon.shaped import Contour
+from tests.utils import equivalence
+from . import strategies
+
+
+@given(strategies.contours)
+def test_vertices(contour: Contour) -> None:
+    assert all(vertex in contour
+               for vertex in contour.vertices)
+
+
+@given(strategies.contours_with_points)
+def test_indexing(contour_with_point: Tuple[Contour, Point]) -> None:
+    contour, point = contour_with_point
+
+    before_indexing = point in contour
+
+    contour.index()
+
+    after_indexing = point in contour
+
+    assert equivalence(before_indexing, after_indexing)
