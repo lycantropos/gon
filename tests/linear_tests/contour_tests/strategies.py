a1e900f8188bf689abe2f5bb89f73fae67f1301f
Azat Ibrakov <azatibrakov@gmail.com>
2020-05-18 21:19:27 +0300

    Add test for 'Contour.__contains__' magic method

--- tests/linear_tests/contour_tests/strategies.py
+++ tests/linear_tests/contour_tests/strategies.py
@@ -1,16 +1,29 @@
+from typing import Tuple
+
+from hypothesis import strategies
 from hypothesis_geometry import planar
 
+from gon.hints import Coordinate
 from gon.linear import Contour
+from gon.primitive import Point
 from tests.strategies import (contours_with_repeated_points,
-                              coordinates_strategies,
-                              coordinates_to_contours,
-                              invalid_vertices_contours,
+                              coordinates_strategies, coordinates_to_contours,
+                              coordinates_to_points, invalid_vertices_contours,
                               small_contours)
-from tests.utils import (to_pairs,
-                         to_triplets)
+from tests.utils import (Strategy, to_pairs, to_triplets)
 
 raw_contours = coordinates_strategies.flatmap(planar.contours)
-contours = raw_contours.map(Contour.from_raw)
+contours = coordinates_strategies.flatmap(coordinates_to_contours)
+
+
+def coordinates_to_contours_with_points(coordinates: Strategy[Coordinate]
+                                        ) -> Strategy[Tuple[Contour, Point]]:
+    return strategies.tuples(coordinates_to_contours(coordinates),
+                             coordinates_to_points(coordinates))
+
+
+contours_with_points = (coordinates_strategies
+                        .flatmap(coordinates_to_contours_with_points))
 invalid_contours = (small_contours
                     | invalid_vertices_contours
                     | contours_with_repeated_points)
