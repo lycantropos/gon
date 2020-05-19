4967e4840c057eab032312cf0072adee9ed8abc5
Azat Ibrakov <azatibrakov@gmail.com>
2020-05-19 17:32:03 +0300

    Add 'Empty' class

--- gon/discrete.py
+++ gon/discrete.py
@@ -10,7 +10,57 @@ from .hints import Domain
 from .primitive import (Point,
                         RawPoint)
 
+RawEmpty = type(None)
 RawMultipoint = List[RawPoint]
+
+
+class Empty(Compound):
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
+    __repr__ = generate_repr(__new__)
+
+    def __contains__(self, other: Geometry) -> bool:
+        return False
+
+    def __eq__(self, other: Geometry) -> bool:
+        return self is other
+
+    def __ge__(self, other: Compound) -> bool:
+        return self is other
+
+    def __gt__(self, other: Compound) -> bool:
+        return False
+
+    def __hash__(self) -> int:
+        return 0
+
+    def __le__(self, other: Compound) -> bool:
+        return True
+
+    def __lt__(self, other: Compound) -> bool:
+        return self is not other
+
+    def relate(self, other: Compound) -> Relation:
+        return Relation.DISJOINT
+
+    @classmethod
+    def from_raw(cls, raw: RawEmpty) -> Domain:
+        assert raw is None
+        return cls()
+
+    def raw(self) -> RawEmpty:
+        return None
+
+    def validate(self) -> None:
+        pass
+
+
+EMPTY = Empty()
 
 
 class Multipoint(Compound):
