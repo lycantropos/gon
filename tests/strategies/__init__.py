f435e4a74972d29ea3bba93f0d0bbbf0c412289b
Azat Ibrakov <azatibrakov@gmail.com>
2020-05-19 14:40:42 +0300

    Complete tests for 'Compound' subclasses

--- tests/strategies/__init__.py
+++ tests/strategies/__init__.py
@@ -1,6 +1,6 @@
 from .base import coordinates_strategies
 from .factories import (coordinates_to_contours,
-                        coordinates_to_contours,
+                        coordinates_to_multipoints,
                         coordinates_to_points,
                         coordinates_to_polygons,
                         coordinates_to_segments)
