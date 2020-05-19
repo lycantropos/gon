fd091602de5de9f510e385397312cb79d5b7b8f1
Azat Ibrakov <azatibrakov@gmail.com>
2020-05-19 15:35:34 +0300

    Add test for 'Multipoint.__hash__' magic method

---
+++ tests/discrete_tests/multipoint_tests/test_hash.py
@@ -0,0 +1,23 @@
+from typing import Tuple
+
+from hypothesis import given
+
+from gon.discrete import Multipoint
+from tests.utils import implication
+from . import strategies
+
+
+@given(strategies.multipoints)
+def test_determinism(multipoint: Multipoint) -> None:
+    result = hash(multipoint)
+
+    assert result == hash(multipoint)
+
+
+@given(strategies.multipoints_pairs)
+def test_connection_with_equality(multipoints_pair: Tuple[Multipoint,
+                                                          Multipoint]) -> None:
+    left_multipoint, right_multipoint = multipoints_pair
+
+    assert implication(left_multipoint == right_multipoint,
+                       hash(left_multipoint) == hash(right_multipoint))
