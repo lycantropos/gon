30cdcda33d246ef50c3da82397423bc3f09b2519
Azat Ibrakov <azatibrakov@gmail.com>
2020-05-19 14:53:26 +0300

    Complete strategies

--- tests/strategies/__init__.py
+++ tests/strategies/__init__.py
@@ -4,9 +4,7 @@ from .factories import (coordinates_to_contours,
                         coordinates_to_points,
                         coordinates_to_polygons,
                         coordinates_to_segments)
-from .linear import (contours_with_repeated_points,
-                     invalid_vertices_contours,
-                     small_contours)
 from .primitive import (invalid_points,
                         points,
-                        points_strategies)
+                        points_strategies,
+                        repeated_raw_points)
