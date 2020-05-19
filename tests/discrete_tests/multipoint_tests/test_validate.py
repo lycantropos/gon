195dea5255ff93525435d244b9d729b9ee865d19
Azat Ibrakov <azatibrakov@gmail.com>
2020-05-19 15:35:59 +0300

    Add test for 'Multipoint.validate' method

---
+++ tests/discrete_tests/multipoint_tests/test_validate.py
@@ -0,0 +1,18 @@
+import pytest
+from hypothesis import given
+
+from gon.discrete import Multipoint
+from . import strategies
+
+
+@given(strategies.multipoints)
+def test_basic(multipoint: Multipoint) -> None:
+    result = multipoint.validate()
+
+    assert result is None
+
+
+@given(strategies.invalid_multipoints)
+def test_invalid_multipoint(multipoint: Multipoint) -> None:
+    with pytest.raises(ValueError):
+        multipoint.validate()
