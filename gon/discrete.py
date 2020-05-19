5e1687074096a7e5e17991a275623de0f85928d6
Azat Ibrakov <azatibrakov@gmail.com>
2020-05-19 12:43:44 +0300

    Add docstring for 'Multipoint.__le__' magic method

--- gon/discrete.py
+++ gon/discrete.py
@@ -125,6 +125,22 @@ class Multipoint(Compound):
         return hash(self._points_set)
 
     def __le__(self, other: Compound) -> bool:
+        """
+        Checks if the multipoint is a subset of the other geometry.
+
+        Time complexity:
+            ``O(len(self.points))``
+        Memory complexity:
+            ``O(1)``
+
+        >>> multipoint = Multipoint.from_raw([(0, 0), (1, 0), (0, 1)])
+        >>> multipoint <= multipoint
+        True
+        >>> multipoint <= Multipoint.from_raw([(0, 0), (1, 0), (1, 1), (0, 1)])
+        True
+        >>> multipoint <= Multipoint.from_raw([(1, 0), (0, 0), (0, 1)])
+        True
+        """
         return ((self._points_set <= other._points_set
                  if isinstance(other, Multipoint)
                  else other >= self)
