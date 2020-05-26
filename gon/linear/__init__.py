d25fe06e063af43681d79284fb88e7a8620b5d01
Azat Ibrakov <azatibrakov@gmail.com>
2020-05-26 13:49:08 +0300

    Add 'Multisegment' class

--- gon/linear/__init__.py
+++ gon/linear/__init__.py
@@ -1,5 +1,7 @@
 from .contour import Contour
 from .hints import (RawContour,
+                    RawMultisegment,
                     RawSegment,
                     Vertices)
+from .multisegment import Multisegment
 from .segment import Segment
