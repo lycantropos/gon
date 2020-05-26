73efadbe43631b35343ae7b042b6a6d83b5b555e
Azat Ibrakov <azatibrakov@gmail.com>
2020-05-26 16:56:51 +0300

    Complete test for 'Polygon.area' property

--- tests/shaped_tests/polygon_tests/test_area.py
+++ tests/shaped_tests/polygon_tests/test_area.py
@@ -1,22 +1,7 @@
-import math
 from hypothesis import given
 
-from gon.hints import Coordinate
 from gon.shaped import Polygon
 from . import strategies
-
-
-@given(strategies.polygons)
-def test_basic(polygon: Polygon) -> None:
-    assert isinstance(polygon.area, Coordinate)
-
-
-@given(strategies.polygons)
-def test_properties(polygon: Polygon) -> None:
-    result = polygon.area
-
-    assert math.isfinite(result)
-    assert result > 0
 
 
 @given(strategies.polygons)
