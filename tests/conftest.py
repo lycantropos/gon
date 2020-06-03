6ad0f571b7a6770cf068fc2c177f94c3886046db
Azat Ibrakov <azatibrakov@gmail.com>
2020-06-04 01:18:08 +0700

    Complete tests configurations

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,7 +9,7 @@ on_azure_pipelines = os.getenv('TF_BUILD', False)
 is_pypy = platform.python_implementation() == 'PyPy'
 settings.register_profile('default',
                           max_examples=(settings.default.max_examples
-                                        // (4 * (1 + is_pypy))
+                                        // (5 * (1 + is_pypy))
                                         if on_travis_ci or on_azure_pipelines
                                         else settings.default.max_examples),
                           deadline=None,
