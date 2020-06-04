77063683f55916505c7438cf83f50b000cf44adb
Azat Ibrakov <azatibrakov@gmail.com>
2020-06-04 14:29:09 +0700

    Add docstring for 'Multipolygon.__ge__' magic method

--- gon/shaped/multipolygon.py
+++ gon/shaped/multipolygon.py
@@ -110,6 +110,24 @@ class Multipolygon(Indexable, Shaped):
                           else NotImplemented)))
 
     def __ge__(self, other: Compound) -> bool:
+        """
+        Checks if the multipolygon is a superset of the other geometry.
+
+        Time complexity:
+            ``O(vertices_count * log vertices_count)``
+        Memory complexity:
+            ``O(1)``
+
+        where ``vertices_count = sum(len(polygon.border.vertices)\
+ + sum(len(hole.vertices) for hole in polygon.holes)\
+ for polygon in self.polygons)``.
+
+        >>> multipolygon = Multipolygon.from_raw(
+        ...         [([(0, 0), (6, 0), (6, 6), (0, 6)],
+        ...           [[(2, 2), (2, 4), (4, 4), (4, 2)]])])
+        >>> multipolygon >= multipolygon
+        True
+        """
         return (other is EMPTY
                 or self == other
                 or (self.relate(other) in (Relation.EQUAL, Relation.COMPONENT,
