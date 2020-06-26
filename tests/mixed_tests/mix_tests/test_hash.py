400b26b1aca681974767eb87f6bc779f96eb2494
Azat Ibrakov <azatibrakov@gmail.com>
2020-06-26 20:58:06 +0700

    Add test for 'Mix.__hash__' magic method

---
+++ tests/mixed_tests/mix_tests/test_hash.py
@@ -0,0 +1,22 @@
+from typing import Tuple
+
+from hypothesis import given
+
+from gon.mixed import Mix
+from tests.utils import implication
+from . import strategies
+
+
+@given(strategies.mixes)
+def test_determinism(mix: Mix) -> None:
+    result = hash(mix)
+
+    assert result == hash(mix)
+
+
+@given(strategies.mixes_pairs)
+def test_connection_with_equality(mixes_pair: Tuple[Mix, Mix]) -> None:
+    left_mix, right_mix = mixes_pair
+
+    assert implication(left_mix == right_mix,
+                       hash(left_mix) == hash(right_mix))
