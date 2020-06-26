10f12f4532a3c6c384da044b3210ad4fa2d6c9a1
Azat Ibrakov <azatibrakov@gmail.com>
2020-06-26 12:50:32 +0700

    Complete tests configurations

--- tests/utils.py
+++ tests/utils.py
@@ -23,6 +23,8 @@ Domain = TypeVar('Domain')
 Range = TypeVar('Range')
 Strategy = SearchStrategy
 
+MAX_COORDINATE_EXPONENT = 15
+
 
 def equivalence(left_statement: bool, right_statement: bool) -> bool:
     return left_statement is right_statement
