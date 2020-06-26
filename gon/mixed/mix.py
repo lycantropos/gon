92ecbc8672e529721210812c711fdb535ad49700
Azat Ibrakov <azatibrakov@gmail.com>
2020-06-27 00:01:04 +0700

    Add docstring for 'Mix.__sub__' magic method

--- gon/mixed/mix.py
+++ gon/mixed/mix.py
@@ -399,6 +399,35 @@ class Mix(Indexable):
                 & other - self._multipolygon)
 
     def __sub__(self, other: Compound) -> Compound:
+        """
+        Returns difference of the mix with the other geometry.
+
+        Time complexity:
+            ``O(elements_count * log elements_count)``
+        Memory complexity:
+            ``O(1)``
+
+        where ``elements_count = multipoint_size + multisegment_size\
+ + multipolygon_vertices_count``,
+        ``multipoint_size = len(points)``,
+        ``multisegment_size = len(segments)``,
+        ``multipolygon_vertices_count = sum(len(polygon.border.vertices)\
+ + sum(len(hole.vertices) for hole in polygon.holes)\
+ for polygon in polygons)``,
+        ``points = [] if self.multipoint is EMPTY\
+ else self.multipoint.points``,
+        ``segments = [] if self.multisegment is EMPTY\
+ else self.multisegment.segments``,
+        ``polygons = [] if self.multipolygon is EMPTY\
+ else self.multipolygon.polygons``.
+
+        >>> mix = Mix.from_raw(([(3, 3), (7, 7)],
+        ...                     [((0, 6), (0, 8)), ((6, 6), (6, 8))],
+        ...                     [([(0, 0), (6, 0), (6, 6), (0, 6)],
+        ...                       [[(2, 2), (2, 4), (4, 4), (4, 2)]])]))
+        >>> mix - mix is EMPTY
+        True
+        """
         return _from_mix_components(self._multipoint - other,
                                     self._multisegment - other,
                                     self._multipolygon - other)
