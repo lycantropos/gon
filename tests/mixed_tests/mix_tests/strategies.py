45ec468c9ec0871aa4c852574168cd74bc513b12
Azat Ibrakov <azatibrakov@gmail.com>
2020-06-26 21:21:31 +0700

    Add test for 'Mix.__contains__' magic method

--- tests/mixed_tests/mix_tests/strategies.py
+++ tests/mixed_tests/mix_tests/strategies.py
@@ -1,11 +1,16 @@
 from tests.strategies import (coordinates_strategies,
                               coordinates_to_mixes,
+                              coordinates_to_points,
                               coordinates_to_raw_mixes)
-from tests.utils import (to_pairs,
+from tests.utils import (cleave_in_tuples,
+                         to_pairs,
                          to_triplets)
 
 raw_mixes = coordinates_strategies.flatmap(coordinates_to_raw_mixes)
 mixes = coordinates_strategies.flatmap(coordinates_to_mixes)
+mixes_with_points = (coordinates_strategies
+                     .flatmap(cleave_in_tuples(coordinates_to_mixes,
+                                               coordinates_to_points)))
 mixes_strategies = coordinates_strategies.map(coordinates_to_mixes)
 mixes_pairs = mixes_strategies.flatmap(to_pairs)
 mixes_triplets = mixes_strategies.flatmap(to_triplets)
