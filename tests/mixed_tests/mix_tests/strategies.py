76db5d794ef5f99c647c8d26bc10a1337bc128c0
Azat Ibrakov <azatibrakov@gmail.com>
2020-06-26 23:05:24 +0700

    Add test for 'Mix.validate' method

--- tests/mixed_tests/mix_tests/strategies.py
+++ tests/mixed_tests/mix_tests/strategies.py
@@ -1,13 +1,48 @@
+from hypothesis import strategies
+
+from gon.degenerate import Empty
+from gon.mixed import Mix
 from tests.strategies import (coordinates_strategies,
                               coordinates_to_mixes,
+                              coordinates_to_multipoints,
+                              coordinates_to_multipolygons,
+                              coordinates_to_multisegments,
                               coordinates_to_points,
-                              coordinates_to_raw_mixes)
+                              coordinates_to_raw_mixes,
+                              invalid_multipoints,
+                              invalid_multipolygons,
+                              invalid_multisegments)
 from tests.utils import (cleave_in_tuples,
                          to_pairs,
                          to_triplets)
 
 raw_mixes = coordinates_strategies.flatmap(coordinates_to_raw_mixes)
 mixes = coordinates_strategies.flatmap(coordinates_to_mixes)
+empty_geometries = strategies.builds(Empty)
+multipoints = coordinates_strategies.flatmap(coordinates_to_multipoints)
+multisegments = coordinates_strategies.flatmap(coordinates_to_multisegments)
+multipolygons = coordinates_strategies.map(coordinates_to_multipolygons)
+invalid_mixes = (strategies.builds(Mix, empty_geometries | multipoints,
+                                   empty_geometries, empty_geometries)
+                 | strategies.builds(Mix, empty_geometries,
+                                     empty_geometries | multisegments,
+                                     empty_geometries)
+                 | strategies.builds(Mix, empty_geometries, empty_geometries,
+                                     empty_geometries | multipolygons)
+                 | strategies.builds(Mix, invalid_multipoints,
+                                     empty_geometries | multisegments,
+                                     multipolygons)
+                 | strategies.builds(Mix, invalid_multipoints, multisegments,
+                                     empty_geometries | multipolygons)
+                 | strategies.builds(Mix, empty_geometries | multipoints,
+                                     invalid_multisegments, multipolygons)
+                 | strategies.builds(Mix, multipoints, invalid_multisegments,
+                                     empty_geometries | multipolygons)
+                 | strategies.builds(Mix, multipoints,
+                                     empty_geometries | multisegments,
+                                     invalid_multipolygons)
+                 | strategies.builds(Mix, empty_geometries | multipoints,
+                                     multisegments, invalid_multipolygons))
 mixes_with_points = (coordinates_strategies
                      .flatmap(cleave_in_tuples(coordinates_to_mixes,
                                                coordinates_to_points)))
---
+++ tests/mixed_tests/mix_tests/test_validate.py
@@ -0,0 +1,18 @@
+import pytest
+from hypothesis import given
+
+from gon.mixed import Mix
+from . import strategies
+
+
+@given(strategies.mixes)
+def test_basic(mix: Mix) -> None:
+    result = mix.validate()
+
+    assert result is None
+
+
+@given(strategies.invalid_mixes)
+def test_invalid_mix(mix: Mix) -> None:
+    with pytest.raises(ValueError):
+        mix.validate()
