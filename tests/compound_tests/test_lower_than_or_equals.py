d2746d39d43015060f30987314bc1c46ea9ebe1e
Azat Ibrakov <azatibrakov@gmail.com>
2020-06-28 23:31:56 +0700

    Complete test for 'Compound.__le__' magic method

--- tests/compound_tests/test_lower_than_or_equals.py
+++ tests/compound_tests/test_lower_than_or_equals.py
@@ -31,19 +31,15 @@ def test_transitivity(compounds_triplet: Tuple[Compound, Compound, Compound]
 
 
 @given(strategies.compounds_pairs)
-def test_connection_with_lower_than(compounds_pair: Tuple[Compound, Compound]
-                                    ) -> None:
+def test_equivalents(compounds_pair: Tuple[Compound, Compound]) -> None:
     first_compound, second_compound = compounds_pair
 
-    assert implication(first_compound < second_compound,
-                       first_compound <= second_compound)
+    result = first_compound <= second_compound
 
-
-@given(strategies.compounds_pairs)
-def test_connection_with_greater_than_or_equals(compounds_pair: Tuple[Compound,
-                                                                      Compound]
-                                                ) -> None:
-    first_compound, second_compound = compounds_pair
-
-    assert equivalence(first_compound <= second_compound,
-                       second_compound >= first_compound)
+    assert equivalence(result, second_compound >= first_compound)
+    assert equivalence(result,
+                       first_compound < second_compound
+                       or first_compound == second_compound)
+    assert equivalence(result,
+                       second_compound > first_compound
+                       or first_compound == second_compound)
