5580079f7b93b554e4ce73076fdf25c1cacb0cb5
Azat Ibrakov <azatibrakov@gmail.com>
2020-08-22 01:46:37 +0700

    Add test for 'Linear.translate' method

--- tests/linear_tests/strategies.py
+++ tests/linear_tests/strategies.py
@@ -1,5 +1,12 @@
 from tests.strategies import (coordinates_strategies,
-                              coordinates_to_linear_geometries)
+                              coordinates_to_linear_geometries,
+                              rational_coordinates_strategies)
+from tests.utils import (cleave_in_tuples,
+                         identity)
 
 linear_geometries = coordinates_strategies.flatmap(
         coordinates_to_linear_geometries)
+rational_linear_geometries_with_coordinates_pairs = (
+    rational_coordinates_strategies.flatmap(
+            cleave_in_tuples(coordinates_to_linear_geometries, identity,
+                             identity)))
---
+++ tests/linear_tests/test_translate.py
@@ -0,0 +1,17 @@
+from typing import Tuple
+
+from hypothesis import given
+
+from gon.compound import Linear
+from gon.hints import Coordinate
+from . import strategies
+
+
+@given(strategies.rational_linear_geometries_with_coordinates_pairs)
+def test_isometry(linear_with_steps: Tuple[Linear, Coordinate, Coordinate]
+                  ) -> None:
+    linear, step_x, step_y = linear_with_steps
+
+    result = linear.translate(step_x, step_y)
+
+    assert result.length == linear.length
