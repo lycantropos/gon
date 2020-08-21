a086200d51f5aae5f3be6169fc1127dfb170d512
Azat Ibrakov <azatibrakov@gmail.com>
2020-08-22 01:45:17 +0700

    Add test for 'Shaped.translate' method

--- tests/shaped_tests/strategies.py
+++ tests/shaped_tests/strategies.py
@@ -3,10 +3,15 @@ from tests.strategies import (coordinates_strategies,
                               coordinates_to_shaped_geometries,
                               rational_coordinates_strategies)
 from tests.utils import (Strategy,
-                         cleave_in_tuples)
+                         cleave_in_tuples,
+                         identity)
 
 shaped_geometries = (coordinates_strategies
                      .flatmap(coordinates_to_shaped_geometries))
+rational_shaped_geometries_with_coordinates_pairs = (
+    rational_coordinates_strategies.flatmap(
+            cleave_in_tuples(coordinates_to_shaped_geometries, identity,
+                             identity)))
 
 
 def coordinates_to_non_zero_coordinates(coordinates: Strategy[Coordinate]
---
+++ tests/shaped_tests/test_translate.py
@@ -0,0 +1,18 @@
+from typing import Tuple
+
+from hypothesis import given
+
+from gon.compound import Shaped
+from gon.hints import Coordinate
+from . import strategies
+
+
+@given(strategies.rational_shaped_geometries_with_coordinates_pairs)
+def test_isometry(shaped_with_steps: Tuple[Shaped, Coordinate, Coordinate]
+                  ) -> None:
+    shaped, step_x, step_y = shaped_with_steps
+
+    result = shaped.translate(step_x, step_y)
+
+    assert result.area == shaped.area
+    assert result.perimeter == shaped.perimeter
