08c249fb9f2e1e62db26e40296ea155036a5271d
Azat Ibrakov <azatibrakov@gmail.com>
2020-08-23 09:22:42 +0500

    Add test for 'Compound.translate' method

---
+++ tests/compound_tests/test_translate.py
@@ -0,0 +1,17 @@
+from typing import Tuple
+
+from hypothesis import given
+
+from gon.compound import Compound
+from gon.hints import Coordinate
+from . import strategies
+
+
+@given(strategies.rational_non_empty_compounds_with_coordinates_pairs)
+def test_centroid(compound_with_steps: Tuple[Compound, Coordinate, Coordinate]
+                  ) -> None:
+    compound, step_x, step_y = compound_with_steps
+
+    result = compound.translate(step_x, step_y)
+
+    assert result.centroid == compound.centroid.translate(step_x, step_y)
