eea6fdf0e547fc9e4179aba7168b3c1dd07716b8
Azat Ibrakov <azatibrakov@gmail.com>
2020-08-24 09:42:58 +0500

    Complete 'Polygon.locate' method

--- gon/shaped/polygon.py
+++ gon/shaped/polygon.py
@@ -631,7 +631,7 @@ class Polygon(Indexable, Shaped):
         >>> polygon.locate(Point(7, 0)) is Location.EXTERIOR
         True
         """
-        return isinstance(point, Point) and self._raw_locate(point.raw())
+        return self._raw_locate(point.raw())
 
     def raw(self) -> RawPolygon:
         """
