096a061498df1938619c60c27a55c854763eb413
Azat Ibrakov <azatibrakov@gmail.com>
2020-08-25 17:03:23 +0500

    Complete test for 'Geometry.rotate' method

--- tests/geometry_tests/test_rotate.py
+++ tests/geometry_tests/test_rotate.py
@@ -52,15 +52,17 @@ def test_round_trip(geometry_point_with_cosine_sine
     assert result.rotate(cosine, -sine, point) == geometry
 
 
-@given(strategies.rational_geometries_points_with_cosines_sines)
-def test_double_angle(geometry_point_with_cosine_sine
-                      : Tuple[Tuple[Geometry, Point],
-                              Tuple[Coordinate, Coordinate]]) -> None:
-    (geometry, point), (cosine, sine) = geometry_point_with_cosine_sine
+@given(strategies.rational_geometries_points_with_cosines_sines_pairs)
+def test_consecutive(geometry_point_with_cosine_sine_pair
+                     : Tuple[Tuple[Geometry, Point],
+                             Tuple[Coordinate, Coordinate],
+                             Tuple[Coordinate, Coordinate]]) -> None:
+    ((geometry, point), (first_cosine, first_sine),
+     (second_cosine, second_sine)) = geometry_point_with_cosine_sine_pair
 
-    result = geometry.rotate(cosine, sine, point)
+    result = geometry.rotate(first_cosine, first_sine, point)
 
-    double_angle_sine = 2 * sine * cosine
-    double_angle_cosine = cosine * cosine - sine * sine
-    assert (result.rotate(cosine, sine, point)
-            == geometry.rotate(double_angle_cosine, double_angle_sine, point))
+    angle_sum_cosine = first_cosine * second_cosine - first_sine * second_sine
+    angle_sum_sine = first_sine * second_cosine + second_sine * first_cosine
+    assert (result.rotate(second_cosine, second_sine, point)
+            == geometry.rotate(angle_sum_cosine, angle_sum_sine, point))
