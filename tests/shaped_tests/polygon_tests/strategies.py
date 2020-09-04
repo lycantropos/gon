4d9b1ed048211d10322fef288e50446b8af79ce2
Azat Ibrakov <azatibrakov@gmail.com>
2020-09-04 13:01:24 +0500

    Complete test for 'Polygon.centroid' property

--- tests/shaped_tests/polygon_tests/strategies.py
+++ tests/shaped_tests/polygon_tests/strategies.py
@@ -4,12 +4,16 @@ from gon.shaped import Polygon
 from tests.strategies import (coordinates_strategies,
                               coordinates_to_points,
                               coordinates_to_polygons,
-                              invalid_polygons)
+                              invalid_polygons,
+                              rational_coordinates_strategies)
 from tests.utils import (cleave_in_tuples,
                          to_pairs,
                          to_triplets)
 
+rational_raw_polygons = (rational_coordinates_strategies
+                         .flatmap(planar.polygons))
 raw_polygons = coordinates_strategies.flatmap(planar.polygons)
+rational_polygons = rational_raw_polygons.map(Polygon.from_raw)
 polygons = raw_polygons.map(Polygon.from_raw)
 invalid_polygons = invalid_polygons
 polygons_strategies = coordinates_strategies.map(coordinates_to_polygons)
