6cef15d8c7bae9d49225a6f83ae878f1de42941a
Azat Ibrakov <azatibrakov@gmail.com>
2020-09-06 11:20:32 +0500

    Add test for 'Indexable.distance_to' method

--- tests/compound_tests/test_distance_to.py
+++ tests/compound_tests/test_distance_to.py
@@ -2,7 +2,9 @@ from typing import Tuple
 
 from hypothesis import given
 
-from gon.compound import Compound
+from gon.compound import (Compound,
+                          Indexable)
+from gon.geometry import Geometry
 from gon.primitive import Point
 from tests.utils import equivalence
 from . import strategies
@@ -26,3 +28,16 @@ def test_connection_with_disjoint(compounds_pair: Tuple[Compound, Compound]
     result = left_compound.distance_to(right_compound)
 
     assert equivalence(bool(result), left_compound.disjoint(right_compound))
+
+
+@given(strategies.indexables_with_non_empty_geometries)
+def test_indexing(indexable_with_geometry: Tuple[Indexable, Geometry]) -> None:
+    indexable, geometry = indexable_with_geometry
+
+    before_indexing = indexable.distance_to(geometry)
+
+    indexable.index()
+
+    after_indexing = indexable.distance_to(geometry)
+
+    assert before_indexing == after_indexing
