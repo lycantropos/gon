7ba9523b729303d4b62051cb2956e50137b1efd1
Azat Ibrakov <azatibrakov@gmail.com>
2020-09-06 10:10:08 +0500

    Add test for 'Compound.distance_to' method

---
+++ tests/compound_tests/test_distance_to.py
@@ -0,0 +1,28 @@
+from typing import Tuple
+
+from hypothesis import given
+
+from gon.compound import Compound
+from gon.primitive import Point
+from tests.utils import equivalence
+from . import strategies
+
+
+@given(strategies.compounds_with_points)
+def test_connection_with_contains(compound_with_point: Tuple[Compound, Point]
+                                  ) -> None:
+    compound, point = compound_with_point
+
+    result = compound.distance_to(point)
+
+    assert equivalence(not result, point in compound)
+
+
+@given(strategies.non_empty_compounds_pairs)
+def test_connection_with_disjoint(compounds_pair: Tuple[Compound, Compound]
+                                  ) -> None:
+    left_compound, right_compound = compounds_pair
+
+    result = left_compound.distance_to(right_compound)
+
+    assert equivalence(bool(result), left_compound.disjoint(right_compound))
