2e765ad031cc25324a567c35f577709e0da15a75
Azat Ibrakov <azatibrakov@gmail.com>
2020-09-10 21:32:01 +0500

    Add test for 'Contour.scale' method

--- tests/linear_tests/contour_tests/strategies.py
+++ tests/linear_tests/contour_tests/strategies.py
@@ -9,8 +9,11 @@ from gon.primitive import Point
 from tests.strategies import (coordinates_strategies,
                               coordinates_to_contours,
                               coordinates_to_points,
-                              invalid_contours)
+                              invalid_contours,
+                              to_non_zero_coordinates,
+                              to_zero_coordinates)
 from tests.utils import (Strategy,
+                         cleave_in_tuples,
                          to_pairs,
                          to_triplets)
 
@@ -27,6 +30,9 @@ def coordinates_to_contours_with_points(coordinates: Strategy[Coordinate]
 contours_with_points = (coordinates_strategies
                         .flatmap(coordinates_to_contours_with_points))
 invalid_contours = invalid_contours
+contours_with_zero_non_zero_coordinates = coordinates_strategies.flatmap(
+        cleave_in_tuples(coordinates_to_contours, to_zero_coordinates,
+                         to_non_zero_coordinates))
 contours_strategies = coordinates_strategies.map(coordinates_to_contours)
 contours_pairs = contours_strategies.flatmap(to_pairs)
 contours_triplets = contours_strategies.flatmap(to_triplets)
