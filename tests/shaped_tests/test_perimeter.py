de387e8e7ab04a4bd0f46596193b041ab538d571
Azat Ibrakov <azatibrakov@gmail.com>
2021-04-08 11:11:00 +0300

    Complete test for 'Shaped.perimeter' property

--- tests/shaped_tests/test_perimeter.py
+++ tests/shaped_tests/test_perimeter.py
@@ -1,20 +1,17 @@
-import math
-
 from hypothesis import given
+from symba.base import Expression
 
 from gon.compound import Shaped
-from gon.hints import Coordinate
 from . import strategies
 
 
 @given(strategies.shaped_geometries)
 def test_basic(shaped: Shaped) -> None:
-    assert isinstance(shaped.perimeter, Coordinate)
+    assert isinstance(shaped.perimeter, Expression)
 
 
 @given(strategies.shaped_geometries)
 def test_properties(shaped: Shaped) -> None:
     result = shaped.perimeter
 
-    assert math.isfinite(result)
     assert result > 0
