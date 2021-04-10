49a951f99a53a5114d7c12160b36c2f1f0ff7992
Azat Ibrakov <azatibrakov@gmail.com>
2021-04-11 00:51:06 +0300

    Add 'raw' module

---
+++ gon/raw.py
@@ -0,0 +1,21 @@
+from gon.degenerate import (RAW_EMPTY,
+                            RawEmpty)
+from gon.discrete import RawMultipoint
+from gon.linear import (RawContour,
+                        RawMultisegment,
+                        RawSegment)
+from gon.mixed import RawMix
+from gon.primitive import RawPoint
+from gon.shaped import (RawMultipolygon,
+                        RawPolygon)
+
+RAW_EMPTY = RAW_EMPTY
+RawContour = RawContour
+RawEmpty = RawEmpty
+RawMix = RawMix
+RawMultipoint = RawMultipoint
+RawMultipolygon = RawMultipolygon
+RawMultisegment = RawMultisegment
+RawPoint = RawPoint
+RawPolygon = RawPolygon
+RawSegment = RawSegment
