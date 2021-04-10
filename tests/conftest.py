a45f5caa46c12bfd412f68b19776e3efe2e46467
Azat Ibrakov <azatibrakov@gmail.com>
2021-04-11 00:09:20 +0300

    Complete tests configurations

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,31 +1,16 @@
 import os
 import platform
-from decimal import (getcontext,
-                     setcontext)
 
-import pytest
 from hypothesis import (HealthCheck,
                         settings)
 
-from .utils import MAX_COORDINATE_EXPONENT
-
-on_travis_ci = os.getenv('CI', False)
-on_azure_pipelines = os.getenv('TF_BUILD', False)
+on_azure_pipelines = bool(os.getenv('TF_BUILD', False))
 is_pypy = platform.python_implementation() == 'PyPy'
 settings.register_profile('default',
                           max_examples=(settings.default.max_examples
                                         // (10 * (1 + is_pypy))
-                                        if on_travis_ci or on_azure_pipelines
+                                        if on_azure_pipelines
                                         else settings.default.max_examples),
                           deadline=None,
                           suppress_health_check=[HealthCheck.filter_too_much,
                                                  HealthCheck.too_slow])
-
-
-@pytest.fixture(autouse=True,
-                scope='session')
-def set_decimal_context() -> None:
-    # required for accurate calculation of linear length/shaped perimeter
-    context = getcontext()
-    context.prec = MAX_COORDINATE_EXPONENT * 5
-    setcontext(context)
