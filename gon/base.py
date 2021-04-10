b17f02c95a0582d71ca6317abd6e35be3fa09add
Azat Ibrakov <azatibrakov@gmail.com>
2021-04-11 00:50:56 +0300

    Add 'base' module

---
+++ gon/base.py
@@ -0,0 +1,37 @@
+from gon.angular import Orientation
+from gon.compound import (Compound,
+                          Indexable,
+                          Linear,
+                          Location,
+                          Relation,
+                          Shaped)
+from gon.degenerate import (EMPTY,
+                            Empty)
+from gon.discrete import (Multipoint)
+from gon.geometry import Geometry
+from gon.linear import (Contour,
+                        Multisegment,
+                        Segment)
+from gon.mixed import Mix
+from gon.primitive import Point
+from gon.shaped import (Multipolygon,
+                        Polygon)
+
+EMPTY = EMPTY
+Compound = Compound
+Contour = Contour
+Empty = Empty
+Geometry = Geometry
+Indexable = Indexable
+Linear = Linear
+Location = Location
+Mix = Mix
+Multipoint = Multipoint
+Multisegment = Multisegment
+Multipolygon = Multipolygon
+Orientation = Orientation
+Point = Point
+Polygon = Polygon
+Relation = Relation
+Segment = Segment
+Shaped = Shaped
