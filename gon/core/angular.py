9eae5b5a2fff2e81a7a7c1e6e4591d9c3c4971fe
Azat Ibrakov <azatibrakov@gmail.com>
2021-04-25 20:42:30 +0300

    Complete 'to_orientation' function

--- gon/core/angular.py
+++ gon/core/angular.py
@@ -1,14 +1,13 @@
-from robust.angular import (Orientation,
-                            orientation as _orientation)
+from ground.base import (Orientation,
+                         get_context)
 
 from .point import Point
 
 Orientation = Orientation
 
 
-def to_orientation(first_ray_point: Point,
-                   vertex: Point,
+def to_orientation(vertex: Point,
+                   first_ray_point: Point,
                    second_ray_point: Point) -> Orientation:
-    return _orientation(first_ray_point.raw(),
-                        vertex.raw(),
-                        second_ray_point.raw())
+    return get_context().angle_orientation(vertex, first_ray_point,
+                                           second_ray_point)
