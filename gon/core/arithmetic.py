323d5e62b28cbbba819efeed2937084be90e0e62
Azat Ibrakov <azatibrakov@gmail.com>
2021-04-27 18:24:21 +0300

    Complete utilities

--- gon/core/arithmetic.py
+++ gon/core/arithmetic.py
@@ -1,19 +1,6 @@
-from fractions import Fraction
 from typing import Iterable
 
-from symba.base import sqrt
-
 from .hints import Coordinate
-
-
-def robust_divide(dividend: Coordinate, divisor: int) -> Coordinate:
-    return (Fraction(dividend, divisor)
-            if isinstance(dividend, int)
-            else dividend / divisor)
-
-
-sqrt = sqrt
-ZERO = sqrt(0)
 
 
 def non_negative_min(numbers: Iterable[Coordinate]) -> Coordinate:
