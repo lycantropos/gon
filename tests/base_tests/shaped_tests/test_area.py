aa928767de8a7382859ac022bc4abce52cfd649b
Azat Ibrakov <azatibrakov@gmail.com>
2021-04-27 19:16:32 +0300

    Complete tests

--- tests/base_tests/shaped_tests/test_area.py
+++ tests/base_tests/shaped_tests/test_area.py
@@ -1,15 +1,15 @@
 import math
-from numbers import Real
 
 from hypothesis import given
 
 from gon.base import Shaped
+from tests.utils import is_scalar
 from . import strategies
 
 
 @given(strategies.shaped_geometries)
 def test_basic(shaped: Shaped) -> None:
-    assert isinstance(shaped.area, Real)
+    assert is_scalar(shaped.area)
 
 
 @given(strategies.shaped_geometries)
