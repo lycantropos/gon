978730adb365348bc0397e1ed9974c5a020e7ba0
Azat Ibrakov <azatibrakov@gmail.com>
2021-04-27 18:33:30 +0300

    Complete test for 'Polygon.triangulate' method

--- tests/base_tests/polygon_tests/test_triangulate.py
+++ tests/base_tests/polygon_tests/test_triangulate.py
@@ -1,3 +1,6 @@
+from functools import reduce
+from operator import or_
+
 from hypothesis import given
 
 from gon.base import (Polygon,
@@ -10,3 +13,11 @@ def test_basic(polygon: Polygon) -> None:
     result = polygon.triangulate()
 
     assert isinstance(result, Triangulation)
+
+
+@given(strategies.polygons)
+def test_round_trip(polygon: Polygon) -> None:
+    result = polygon.triangulate()
+
+    assert (reduce(or_, [Polygon(contour) for contour in result.triangles()])
+            == polygon)
