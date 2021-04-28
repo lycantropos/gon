cad9dbc5134181155d8eaa0498632a44c106b3fc
Azat Ibrakov <azatibrakov@gmail.com>
2021-04-28 15:48:47 +0300

    Complete test for 'Compound.__contains__' method

--- tests/base_tests/compound_tests/test_contains.py
+++
@@ -1,9 +0,0 @@
-from hypothesis import given
-
-from gon.base import Compound
-from . import strategies
-
-
-@given(strategies.compounds)
-def test_regularity(compound: Compound) -> None:
-    assert compound not in compound
