c9b017dc2ef6c521c45096252e489c067b9ffba3
Azat Ibrakov <azatibrakov@gmail.com>
2021-05-17 17:38:46 +0300

    Complete imports

--- tests/base_tests/linear_tests/test_rotate.py
+++ tests/base_tests/linear_tests/test_rotate.py
@@ -4,13 +4,13 @@ from hypothesis import given
 
 from gon.base import (Linear,
                       Point)
-from gon.hints import Coordinate
+from gon.hints import Scalar
 from . import strategies
 
 
 @given(strategies.rational_linear_geometries_points_with_cosines_sines)
 def test_isometry(linear_with_cosine_sine: Tuple[Tuple[Linear, Point],
-                                                 Tuple[Coordinate, Coordinate]]
+                                                 Tuple[Scalar, Scalar]]
                   ) -> None:
     (linear, point), (cosine, sine) = linear_with_cosine_sine
 
