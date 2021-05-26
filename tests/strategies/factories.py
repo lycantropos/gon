a478f6f280ef227e71b4de30b7f0eaf2aebd7720
Azat Ibrakov <azatibrakov@gmail.com>
2021-05-26 07:59:17 +0300

    Complete strategies

--- tests/strategies/factories.py
+++ tests/strategies/factories.py
@@ -65,19 +65,6 @@ coordinates_to_multisegments = partial(planar.multisegments,
                                        max_size=MAX_LINEAR_SIZE)
 coordinates_to_contours = partial(planar.contours,
                                   max_size=MAX_LINEAR_SIZE)
-
-
-def coordinates_to_mixes(coordinates: Strategy[Scalar]) -> Strategy[Mix]:
-    return ((planar.mixes(coordinates,
-                          min_points_size=1,
-                          min_segments_size=1)
-             | planar.mixes(coordinates,
-                            min_points_size=1,
-                            min_polygons_size=1)
-             | planar.mixes(coordinates,
-                            min_segments_size=1,
-                            min_polygons_size=1)))
-
-
+coordinates_to_mixes = planar.mixes
 coordinates_to_polygons = planar.polygons
 coordinates_to_multipolygons = planar.multipolygons
