899921da59882e37d1db7782adfbe15932c71bc1
Azat Ibrakov <azatibrakov@gmail.com>
2021-07-09 16:56:34 +0300

    Add test for 'Vector.__add__' magic method

--- tests/base_tests/vector_tests/strategies.py
+++ tests/base_tests/vector_tests/strategies.py
@@ -8,6 +8,8 @@ from tests.strategies import (angles,
                               invalid_points,
                               points)
 from tests.utils import (cleave_in_tuples,
+                         identity,
+                         pack,
                          to_pairs,
                          to_triplets)
 
@@ -17,6 +19,13 @@ invalid_vectors = (strategies.builds(Vector, invalid_points | points,
 vectors = coordinates_strategies.flatmap(coordinates_to_vectors)
 vectors_strategies = coordinates_strategies.map(coordinates_to_vectors)
 vectors_pairs = vectors_strategies.flatmap(to_pairs)
+zero_vectors_with_vectors = (
+    (coordinates_strategies.flatmap(cleave_in_tuples(coordinates_to_points,
+                                                     coordinates_to_vectors))
+     .map(pack(lambda point, vector: (Vector(point, point), vector)))))
+vectors_pairs_with_scalars = coordinates_strategies.flatmap(
+        cleave_in_tuples(coordinates_to_vectors, coordinates_to_vectors,
+                         identity))
 vectors_triplets = vectors_strategies.flatmap(to_triplets)
 vectors_with_points = (coordinates_strategies
                        .flatmap(cleave_in_tuples(coordinates_to_vectors,
