6de1b126ce1906938fe472be3680a8e3fa332be7
Azat Ibrakov <azatibrakov@gmail.com>
2022-03-28 20:46:26 +0700

    Drop 'Python3.6.1' support

--- setup.py
+++ setup.py
@@ -28,5 +28,5 @@ setup(name=gon.__name__,
       license='MIT License',
       url=project_base_url,
       download_url=project_base_url + 'archive/master.zip',
-      python_requires='>=3.6',
+      python_requires='>=3.6.2',
       install_requires=Path('requirements.txt').read_text(encoding='utf-8'))
