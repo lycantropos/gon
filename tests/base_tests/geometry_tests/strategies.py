297faa968fdaccae9e4db3adffeb599d8644b79c
Azat Ibrakov <azatibrakov@gmail.com>
2022-03-30 01:38:23 +0700

    Complete strategies

--- tests/base_tests/geometry_tests/strategies.py
+++ tests/base_tests/geometry_tests/strategies.py
@@ -1,3 +1,5 @@
+from itertools import repeat
+
 from hypothesis import strategies
 
 from gon.base import EMPTY
@@ -13,11 +15,11 @@ from tests.strategies import (angles,
                               coordinates_to_segments,
                               to_non_zero_coordinates,
                               to_zero_coordinates)
-from tests.utils import (call,
-                         cleave_in_tuples,
+from tests.utils import (cleave_in_tuples,
+                         combine_factories,
+                         factories_to_values,
                          identity,
-                         to_constant,
-                         to_pairs)
+                         to_constant)
 
 empty_compounds = strategies.just(EMPTY)
 empty_compounds_factory = to_constant(empty_compounds)
@@ -28,50 +30,41 @@ non_empty_compounds_factories = (
                              coordinates_to_contours,
                              coordinates_to_polygons,
                              coordinates_to_multipolygons,
-                             coordinates_to_mixes]))
+                             coordinates_to_mixes])
+)
 non_empty_geometries_factories = (strategies.just(coordinates_to_points)
                                   | non_empty_compounds_factories)
 geometries_factories = (strategies.just(empty_compounds_factory)
                         | non_empty_geometries_factories)
-geometries_strategies = strategies.builds(call, geometries_factories,
-                                          coordinates_strategies)
-geometries = geometries_strategies.flatmap(identity)
-geometries_with_non_zero_coordinates_pairs = strategies.builds(
-        call,
-        geometries_factories.map(lambda factory
-                                 : cleave_in_tuples(factory,
-                                                    to_non_zero_coordinates,
-                                                    to_non_zero_coordinates)),
-        coordinates_strategies).flatmap(identity)
+geometries = factories_to_values(geometries_factories, coordinates_strategies)
+geometries_with_non_zero_coordinates_pairs = factories_to_values(
+        combine_factories(geometries_factories,
+                          *repeat(strategies.just(to_non_zero_coordinates),
+                                  times=2)),
+        coordinates_strategies
+)
 empty_compounds_with_coordinates_pairs = (
     coordinates_strategies.flatmap(cleave_in_tuples(empty_compounds_factory,
-                                                    identity, identity)))
-geometries_with_coordinates_pairs = strategies.builds(
-        call,
-        geometries_factories.map(lambda factory
-                                 : cleave_in_tuples(factory,
-                                                    to_zero_coordinates,
-                                                    identity))
-        | geometries_factories.map(lambda factory
-                                   : cleave_in_tuples(factory, identity,
-                                                      to_zero_coordinates))
-        | geometries_factories.map(lambda factory
-                                   : cleave_in_tuples(factory, identity,
-                                                      identity)),
-        coordinates_strategies).flatmap(identity)
-geometries_with_points = (
-    (strategies.builds(call,
-                       geometries_factories
-                       .map(lambda factory
-                            : cleave_in_tuples(factory,
-                                               coordinates_to_points)),
-                       coordinates_strategies)
-     .flatmap(identity)))
+                                                    identity, identity))
+)
+geometries_with_coordinates_pairs = factories_to_values(
+        combine_factories(geometries_factories,
+                          *repeat(strategies.sampled_from([to_zero_coordinates,
+                                                           identity]),
+                                  times=2)),
+        coordinates_strategies
+)
+geometries_with_points = factories_to_values(
+        combine_factories(geometries_factories,
+                          strategies.just(coordinates_to_points)),
+        coordinates_strategies
+)
 geometries_points_with_angles = strategies.tuples(geometries_with_points,
                                                   angles)
 geometries_points_with_angles_pairs = strategies.tuples(geometries_with_points,
                                                         angles, angles)
-non_empty_geometries_pairs = (strategies.builds(call,
-                                                non_empty_geometries_factories,
-                                                coordinates_strategies)
-                              .flatmap(to_pairs))
+non_empty_geometries_pairs = factories_to_values(
+        combine_factories(*repeat(non_empty_geometries_factories,
+                                  times=2)),
+        coordinates_strategies
+)
